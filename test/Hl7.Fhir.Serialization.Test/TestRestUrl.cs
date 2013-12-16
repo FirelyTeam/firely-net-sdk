0c58e98f0da097f849143b4ce65bef838ce3c29e
unknown <martijnh@Colorado.furore.com>
2013-12-16 17:55:35 +0100

    Rewritten sourceLocation, Added RestUrl

---
+++ test/Hl7.Fhir.Serialization.Test/TestRestUrl.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Hl7.Fhir.Support;
+using Hl7.Fhir.Rest;
+
+namespace Hl7.Fhir.Test
+{
+    [TestClass]
+    public class TestRestUrl
+    {
+        [TestMethod]
+        public void CreateFromEndPoint()
+        {
+            Endpoint endpoint = new Endpoint("http://localhost/fhir");
+            RestUrl resturi;
+
+            resturi = endpoint.Collection("patient");
+            Assert.AreEqual("http://localhost/fhir/patient", resturi.AsString);
+
+            resturi = endpoint.Resource("patient", "1");
+            Assert.AreEqual("http://localhost/fhir/patient/1", resturi.AsString);
+
+            resturi = endpoint.Resource("patient", "1");
+            Assert.AreEqual("http://localhost/fhir/patient/1", resturi.AsString);
+        }
+
+        [TestMethod]
+        public void Query()
+        {
+            Endpoint endpoint = new Endpoint("http://localhost/fhir/");
+            RestUrl resturi;
+            
+            resturi = endpoint.Search("organization").Param("family", "Johnson").Param("given", "William");
+            Assert.AreEqual("http://localhost/fhir/organization/_search?family=Johnson&given=William", resturi.AsString);
+
+        }
+    }
+}
