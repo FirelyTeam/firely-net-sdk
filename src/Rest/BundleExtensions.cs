26ff54ec2c3fe7c81e83b6abb16b0859459f50ad
Ewout Kramer <e.kramer@furore.com>
2014-01-10 18:48:35 +0100

    Small refactorings, bug fixes, addition of RefreshBundle

---
+++ src/Rest/BundleExtensions.cs
@@ -0,0 +1,56 @@
+﻿using Hl7.Fhir.Model;
+using Hl7.Fhir.Serialization;
+using Hl7.Fhir.Support;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Hl7.Fhir.Rest
+{
+    public static class BundleExtensions
+    {
+        public static Bundle RefreshBundle(this FhirClient client, Bundle bundle)
+        {
+            if (bundle == null) throw Error.ArgumentNull("bundle");
+
+            // Clone old bundle, without the entries (so, just the header)
+            var oldEntries = bundle.Entries;
+            Bundle result;
+
+            try
+            {
+                bundle.Entries = new List<BundleEntry>();
+                var xml = FhirSerializer.SerializeBundleToXml(bundle);
+                result = FhirParser.ParseBundleFromXml(xml);
+            }
+            catch
+            {
+                throw;
+            }
+            finally
+            {
+                bundle.Entries = oldEntries;
+            }
+
+            result.Id = new Uri("urn:uuid:" + Guid.NewGuid().ToString());
+            result.LastUpdated = DateTimeOffset.Now;
+            result.Entries = new List<BundleEntry>();
+            foreach (var entry in bundle.Entries)
+            {
+                if (entry is ResourceEntry)
+                {
+                    var newEntry = client.Read(entry.Id);
+                    if (entry.Links.Related != null) newEntry.Links.Related = entry.Links.Related;
+                    result.Entries.Add(newEntry);
+                }
+                else if (entry is DeletedEntry)
+                    result.Entries.Add(entry);
+                else
+                    throw Error.NotSupported("Cannot refresh an entry of type {0}", messageArgs: entry.GetType().Name);
+            }
+
+            return result;
+        }
+    }
+}
--- src/Rest/RestUrl.cs
+++ src/Rest/RestUrl.cs
@@ -27,7 +27,7 @@ namespace Hl7.Fhir.Rest
             _builder = new UriBuilder(url);
 
             if (!String.IsNullOrEmpty(_builder.Query))
-                _parameters = new List<Tuple<string,string>>( QueryParam.Split(_builder.Query) ); 
+                _parameters = new List<Tuple<string,string>>( SplitParams(_builder.Query) ); 
         }
 
 
@@ -40,7 +40,7 @@ namespace Hl7.Fhir.Rest
         { 
             get
             {
-                _builder.Query = QueryParam.Join(_parameters);
+                _builder.Query = JoinParams(_parameters);
                 return _builder.Uri;
             } 
         }
@@ -95,12 +95,18 @@ namespace Hl7.Fhir.Rest
 
         public bool IsEndpointFor(Uri other)
         {
-            return this.Uri.IsBaseOf(other);
+            return IsEndpointFor(other.ToString());
         }
 
         public bool IsEndpointFor(string other)
         {
-            var baseUri = new Uri(delimit(this.Uri.ToString()));
+            var baseAddress = this.Uri.ToString();
+
+            // HACK! To support Fiddler2 on Win8, localhost needs to be spelled out as localhost.fiddler, but still functions as localhost
+            baseAddress = baseAddress.Replace("localhost.fiddler", "localhost");
+            var baseUri = new Uri(delimit(baseAddress));
+
+            other = delimit(other.Replace("localhost.fiddler", "localhost"));
 
             return baseUri.IsBaseOf(new Uri(other,UriKind.RelativeOrAbsolute));
         }
@@ -136,5 +142,92 @@ namespace Hl7.Fhir.Rest
         {
             return AsString;
         }
+
+        /// <summary>
+        /// Parses the possibly escaped key=value query parameter into a (key,value) Tuple
+        /// </summary>
+        /// <param name="param"></param>
+        /// <returns>A Tuple<string,string> containing the key and value. Value maybe null if
+        /// only the key was specified as a query parameter.</returns>
+        internal static Tuple<string, string> SplitParam(string param)
+        {
+            if (param == null) throw new ArgumentNullException("param");
+
+            string[] pair = param.Split('=');
+
+            var key = Uri.UnescapeDataString(pair[0]);
+            var value = pair.Length >= 2 ? String.Join("?", pair.Skip(1)) : null;
+            if (value != null) value = Uri.UnescapeDataString(value);
+
+            return new Tuple<string, string>(key, value);
+        }
+
+        public static IEnumerable<Tuple<string, string>> SplitParams(string query)
+        {
+            if (query == null) throw new ArgumentNullException("query");
+
+            var result = new List<Tuple<string, string>>();
+
+            if (query == String.Empty) return result;
+
+            var q = query.TrimStart('?');
+
+            var querySegments = q.Split(new string[] { "&" }, StringSplitOptions.RemoveEmptyEntries);
+
+            foreach (var segment in querySegments)
+            {
+                var kv = SplitParam(segment);
+                result.Add(kv);
+            }
+
+            return result;
+        }
+
+
+        /// <summary>
+        /// Converts a key,value pair into a query parameters, escaping the key and value
+        /// of necessary.
+        /// </summary>
+        /// <param name="key"></param>
+        /// <param name="value"></param>
+        /// <returns></returns>
+        internal static string MakeParam(string key, string value = null)
+        {
+            if (key == null) throw new ArgumentNullException("key");
+
+            var result = Uri.EscapeDataString(key);
+
+            if (value != null)
+                result += "=" + Uri.EscapeDataString(value);
+
+            return result;
+        }
+
+
+        internal static string JoinParam(Tuple<string, string> kv)
+        {
+            if (kv == null) throw new ArgumentNullException("kv");
+            if (kv.Item1 == null) throw new ArgumentException("Key in tuple may not be null", "kv");
+
+            return MakeParam(kv.Item1, kv.Item2);
+        }
+
+        /// <summary>
+        /// Builds a query string based on a set of key,value pairs
+        /// </summary>
+        /// <param name="pars"></param>
+        /// <returns></returns>
+        public static string JoinParams(IEnumerable<Tuple<string, string>> pars)
+        {
+            StringBuilder result = new StringBuilder();
+
+            foreach (var kv in pars)
+            {
+                result.Append(JoinParam(kv));
+                result.Append("&");
+            }
+
+            return result.ToString().TrimEnd('&');
+        }    
     }    
 }
