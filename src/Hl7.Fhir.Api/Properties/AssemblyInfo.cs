99e3a3a28914e2126b6e86834404a19c8c894a33
Ewout Kramer <e.kramer@furore.com>
2014-09-11 16:55:34 +0200

    Bumped package version number

--- src/Hl7.Fhir.Api/Properties/AssemblyInfo.cs
+++ src/Hl7.Fhir.Api/Properties/AssemblyInfo.cs
@@ -26,8 +26,7 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("0.11.0.*")]
-[assembly: AssemblyFileVersion("0.11.0.*")]
+[assembly: AssemblyVersion("0.11.1.*")]
 
 #if DEBUG
 [assembly:InternalsVisibleTo("Hl7.Fhir.Test")]
