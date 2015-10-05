39e68fda574ff25efbdd40182aa1fdcd607210b3
Ewout Kramer <e.kramer@furore.com>
2015-10-05 19:40:09 +0200

    Preparing for release

--- src/Hl7.Fhir.Core/Properties/AssemblyInfo.cs
+++ src/Hl7.Fhir.Core/Properties/AssemblyInfo.cs
@@ -7,8 +7,8 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("Hl7.Fhir.Core")]
-[assembly: AssemblyDescription("Core .NET support for working with HL7 FHIR. Supports FHIR DSTU2 (1.0.1)")]
+[assembly: AssemblyTitle("Hl7.Fhir.DSTU2.Core")]
+[assembly: AssemblyDescription("Core .NET support for working with HL7 FHIR. Supports FHIR DSTU2 (1.0)")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyProduct("FHIR")]
 [assembly: AssemblyCopyright("Copyright Ewout Kramer and collaborators 2015")]
@@ -27,4 +27,4 @@ using System.Runtime.InteropServices;
 
 [assembly: CLSCompliant(true)]
 
-[assembly: AssemblyVersion("0.90.1.*")]
+[assembly: AssemblyVersion("0.90.2.*")]
--- src/Hl7.Fhir.Specification/Properties/AssemblyInfo.cs
+++ src/Hl7.Fhir.Specification/Properties/AssemblyInfo.cs
@@ -7,8 +7,8 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("Hl7.Fhir.Specification")]
-[assembly: AssemblyDescription(".NET additional support for working with HL7 FHIR. Supports FHIR DSTU2 (1.0.1).")]
+[assembly: AssemblyTitle("Hl7.Fhir.DSTU2.Specification")]
+[assembly: AssemblyDescription(".NET additional support for working with HL7 FHIR. Supports FHIR DSTU2 (1.0).")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyProduct("Hl7.Fhir.Specification")]
 [assembly: AssemblyCopyright("Copyright © Ewout Kramer and collaborators 2015")]
@@ -27,7 +27,7 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("0.90.1.*")]
+[assembly: AssemblyVersion("0.90.2.*")]
 
 #if DEBUG
 [assembly: InternalsVisibleTo("Hl7.Fhir.Specification.Tests")]
