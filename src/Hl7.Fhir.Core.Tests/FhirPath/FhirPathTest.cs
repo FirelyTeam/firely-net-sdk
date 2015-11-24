75d4174268b4648e06e7316b7d87f1fcc0f65724
Ewout Kramer <e.kramer@furore.com>
2015-11-24 18:24:21 +0100

    Corrected the test expression

--- src/Hl7.Fhir.Core.Tests/FhirPath/FhirPathTest.cs
+++ src/Hl7.Fhir.Core.Tests/FhirPath/FhirPathTest.cs
@@ -28,7 +28,7 @@ namespace Hl7.Fhir.Tests.FhirPath
         [TestMethod]
         public void TestExpression()
         {
-            var result = Expression.Expr.Parse("(4>$parent.bla*.blie.(jee+4).bloe.where[parent>5])and(%bla>=6)");
+            var result = Expression.Expr.Parse("(4>$parent.bla*.blie.(jee+4).bloe.where(parent>5))and(%bla>=6)");
             Assert.AreEqual("(4 > $parent.bla.blie.bloe)and(%bla>=6)", result);
         }
     
