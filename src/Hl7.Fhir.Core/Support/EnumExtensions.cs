59ebef8b9d89ae47a1681799c9f4be9af5d9689c
Ewout Kramer <e.kramer@furore.com>
2016-01-21 11:44:29 +0100

    feature: Added helper methods for converting to/from FHIRDefinedType enum and working with enums in general.

---
+++ src/Hl7.Fhir.Core/Support/EnumExtensions.cs
@@ -0,0 +1,34 @@
+﻿using Hl7.Fhir.Introspection;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Hl7.Fhir.Support
+{
+    public static class EnumExtensions
+    {
+        public static string GetLiteral(this Enum e)
+        {
+            var attr = e.GetAttributeOnEnum<EnumLiteralAttribute>();
+
+            if (attr != null)
+                return attr.Literal;
+            else
+                return null;
+        }
+
+        public static string GetDocumentation(this Enum e)
+        {
+            var attr = e.GetAttributeOnEnum<DescriptionAttribute>();
+
+            if (attr != null)
+                return attr.Description;
+            else
+                return null;
+        }
+
+    }
+}
