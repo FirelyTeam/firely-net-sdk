f170fb3c443737fd5b7e16c7384714c6c827908e
Tilo Christ <skydrive@legentix.com>
2016-01-28 20:06:49 +0100

    Eliminate need for curl, here comes PowerCurl!

--- build/DownloadFhirBuildProfile.ps1
+++ build/DownloadFhirBuildProfile.ps1
@@ -6,21 +6,35 @@ cls
 # Script to be run from build directory
 
 $server = "http://hl7-fhir.github.io";
-$curl = "..\tools\curl\curl.exe";
-$srcdir = "..\src";
+$baseDir = Resolve-Path ..
+$srcdir = "$baseDir\src";
+
+function PowerCurl($targetFile, $sourceUrl)
+{
+	Try
+	{
+        $webclient = New-Object System.Net.WebClient
+        $webclient.DownloadFile($sourceUrl, $targetFile)
+	} Catch
+	{
+		Write-Host "$_ occurred while downloading $sourceUrl to $targetFile"
+		$_ | fl * -Force
+	}
+}
+
 
 # Download the core profiles for code generation
-&$curl -o "$srcdir\Hl7.Fhir.Core\Model\Source\expansions.xml" "$server/expansions.xml"
-&$curl -o "$srcdir\Hl7.Fhir.Core\Model\Source\profiles-resources.xml" "$server/profiles-resources.xml"
-&$curl -o "$srcdir\Hl7.Fhir.Core\Model\Source\profiles-types.xml" "$server/profiles-types.xml"
-&$curl -o "$srcdir\Hl7.Fhir.Core\Model\Source\search-parameters.xml" "$server/search-parameters.xml"
+PowerCurl "$srcdir\Hl7.Fhir.Core\Model\Source\expansions.xml" "$server/expansions.xml"
+PowerCurl "$srcdir\Hl7.Fhir.Core\Model\Source\profiles-resources.xml" "$server/profiles-resources.xml"
+PowerCurl "$srcdir\Hl7.Fhir.Core\Model\Source\profiles-types.xml" "$server/profiles-types.xml"
+PowerCurl "$srcdir\Hl7.Fhir.Core\Model\Source\search-parameters.xml" "$server/search-parameters.xml"
 
-&$curl -o "$srcdir\Hl7.Fhir.Core.Tests\TestData\careplan-example-f201-renal.xml" "$server/careplan-example-f201-renal.xml"
-&$curl -o "$srcdir\Hl7.Fhir.Core.Tests\TestData\examples.zip" "$server/examples.zip"
-&$curl -o "$srcdir\Hl7.Fhir.Core.Tests\TestData\examples-json.zip" "$server/examples-json.zip"
-&$curl -o "$srcdir\Hl7.Fhir.Core.Tests\TestData\json-edge-cases.json" "$server/json-edge-cases.json"
+PowerCurl "$srcdir\Hl7.Fhir.Core.Tests\TestData\careplan-example-f201-renal.xml" "$server/careplan-example-f201-renal.xml"
+PowerCurl "$srcdir\Hl7.Fhir.Core.Tests\TestData\examples.zip" "$server/examples.zip"
+PowerCurl "$srcdir\Hl7.Fhir.Core.Tests\TestData\examples-json.zip" "$server/examples-json.zip"
+PowerCurl "$srcdir\Hl7.Fhir.Core.Tests\TestData\json-edge-cases.json" "$server/json-edge-cases.json"
 
-&$curl -o "$srcdir\Hl7.Fhir.Specification\validation.xml.zip" "$server/validation.xml.zip"
-&$curl -o "$srcdir\Hl7.Fhir.Specification.Tests\TestData\snapshot-test\profiles-others.xml" "$server/profiles-others.xml"
+PowerCurl "$srcdir\Hl7.Fhir.Specification\validation.xml.zip" "$server/validation.xml.zip"
+PowerCurl "$srcdir\Hl7.Fhir.Specification.Tests\TestData\snapshot-test\profiles-others.xml" "$server/profiles-others.xml"
 copy "$srcdir\Hl7.Fhir.Core\Model\Source\profiles-resources.xml" "$srcdir\Hl7.Fhir.Specification.Tests\TestData\snapshot-test"
 copy "$srcdir\Hl7.Fhir.Core\Model\Source\profiles-types.xml" "$srcdir\Hl7.Fhir.Specification.Tests\TestData\snapshot-test"
