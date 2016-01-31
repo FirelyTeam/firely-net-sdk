0bdc3591e1fb915d64b2860928b2fd2bcaf7ff35
Tilo Christ <skydrive@legentix.com>
2016-01-31 12:16:21 +0100

    Amendment to PR #162: Reenable VB case ambiguity warnings.
    Hl7.Fhir.Specification is fully CLSCompliant. I.e. assembly flagged as CLSCompliant true and no need to suppress any warnings.
    
    Hl7.Fhir.Core: Mark the 2 offending classes specifically as non-compliant. Only suppress warning CS3016, as CS3015 no longer occurs. Mark overall assembly as CLSCompliant to receive warnings if VB case ambiguity is accidentally introduced.

--- src/Hl7.Fhir.Core/Properties/AssemblyInfo.cs
+++ src/Hl7.Fhir.Core/Properties/AssemblyInfo.cs
@@ -25,6 +25,8 @@ using System.Runtime.InteropServices;
 [assembly:AssemblyKeyFileAttribute("..\\FhirNetApi.snk")]
 #endif
 
-[assembly: CLSCompliant(false)]
+// This assembly is not fully CLSCompliant, but this triggers compiler warnings to avoid the issue as described here, when mixing C# and VB
+// https://msdn.microsoft.com/en-us/library/ms235408(v=vs.90).aspx
+[assembly: CLSCompliant(true)]
 
 [assembly: AssemblyVersion("0.90.6.*")]
