1b5f46aaaa71275559e71028c666e6f595f54d43
Martijn Harthoorn <m.harthoorn@furore.com>
2016-08-04 23:39:10 +0200

    fixed: target platform 4.5 added, removed XmlNs class

--- src/HL7.Fhir.FluentPath/Support/XmlNs.cs
+++ src/HL7.Fhir.FluentPath/Support/XmlNs.cs
@@ -15,28 +15,29 @@ using System.Reflection;
 using System.Text;
 using System.Xml.Linq;
 
-namespace Hl7.Fhir.Support
+namespace Hl7.Fhir.FluentPath.Support
 {
-    public static class XmlNs
-    {
-        public const string FHIR = "http://hl7.org/fhir";
-        public const string FHIR_URL_SEARCHPARAM = FHIR + "/query";
-        public const string FHIRTAG = FHIR + "/tag";
-        public const string TAG_PROFILE = XmlNs.FHIRTAG + "/profile";
-        public const string TAG_SECURITY = XmlNs.FHIRTAG + "/security";
-        public const string ATOM_CATEGORY_RESOURCETYPE = FHIR + "/resource-types";
+    //public static class XmlNs
+    //{
+    //    // todo: this class is a complete copy of Hl7.Fhir.Support.XmlNs in the fhir-net-api.
+    //    public const string FHIR = "http://hl7.org/fhir";
+    //    public const string FHIR_URL_SEARCHPARAM = FHIR + "/query";
+    //    public const string FHIRTAG = FHIR + "/tag";
+    //    public const string TAG_PROFILE = XmlNs.FHIRTAG + "/profile";
+    //    public const string TAG_SECURITY = XmlNs.FHIRTAG + "/security";
+    //    public const string ATOM_CATEGORY_RESOURCETYPE = FHIR + "/resource-types";
 
-        public const string XHTML = "http://www.w3.org/1999/xhtml";
-        public const string ATOMPUB_TOMBSTONES = "http://purl.org/atompub/tombstones/1.0";
-        public const string ATOM = "http://www.w3.org/2005/Atom";
-        public const string OPENSEARCH = "http://a9.com/-/spec/opensearch/1.1/";
-        public const string NAMESPACE = "http://www.w3.org/XML/1998/namespace";
+    //    public const string XHTML = "http://www.w3.org/1999/xhtml";
+    //    public const string ATOMPUB_TOMBSTONES = "http://purl.org/atompub/tombstones/1.0";
+    //    public const string ATOM = "http://www.w3.org/2005/Atom";
+    //    public const string OPENSEARCH = "http://a9.com/-/spec/opensearch/1.1/";
+    //    public const string NAMESPACE = "http://www.w3.org/XML/1998/namespace";
 
 
-        public static readonly XNamespace XATOM = ATOM;
-        public static readonly XNamespace XATOMPUB_TOMBSTONES = ATOMPUB_TOMBSTONES;
-        public static readonly XNamespace XFHIR = FHIR;
-        public static readonly XNamespace XOPENSEARCH = OPENSEARCH;
-        public static readonly XNamespace XHTMLNS = XHTML;
-    }
+    //    public static readonly XNamespace XATOM = ATOM;
+    //    public static readonly XNamespace XATOMPUB_TOMBSTONES = ATOMPUB_TOMBSTONES;
+    //    public static readonly XNamespace XFHIR = FHIR;
+    //    public static readonly XNamespace XOPENSEARCH = OPENSEARCH;
+    //    public static readonly XNamespace XHTMLNS = XHTML;
+    //}
 }
