374c01b24b98fd7170efe2eb1693747fd06bd9b3
Ewout Kramer <e.kramer@furore.com>
2016-08-10 17:59:37 +0200

    Project restructe to have one project and one nuget package

--- src/Sprache/Properties/AssemblyInfo.cs
+++
@@ -1,8 +0,0 @@
-﻿using System.Reflection;
-using System.Runtime.CompilerServices;
-
-[assembly: AssemblyTitle("Sprache Tiny C# Parser Combinators")]
-[assembly: AssemblyProduct("Sprache")]
-[assembly: AssemblyCopyright("Copyright © Nicholas Blumhardt 2010")]
-[assembly: InternalsVisibleTo("Sprache.Tests")]
-[assembly: AssemblyVersion("2.0.0.0")]
