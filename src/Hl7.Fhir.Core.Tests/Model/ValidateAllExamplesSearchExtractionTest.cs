2546bff63358ea0198571547657ea4a79a384cee
Brian Postlethwaite <brianpos@optushome.com.au>
2016-08-15 16:55:52 +1000

    Extract search parameter test values from the bundles and contained resources too

--- src/Hl7.Fhir.Core.Tests/Model/ValidateAllExamplesSearchExtractionTest.cs
+++ src/Hl7.Fhir.Core.Tests/Model/ValidateAllExamplesSearchExtractionTest.cs
@@ -62,43 +62,15 @@ namespace Hl7.Fhir.Tests.Model
                         var reader = SerializationUtil.WrapXmlReader(XmlReader.Create(file));
                         var resource = parser.Parse<Resource>(reader);
 
-                        // Extract the search properties
-                        var searchparameters = ModelInfo.SearchParameters.Where(r => r.Resource == resource.ResourceType.ToString() && !String.IsNullOrEmpty(r.Expression));
-                        foreach (var index in searchparameters)
-                        {
-                            // prepare the search data cache
-                            string key = resource.ResourceType.ToString() + "_" + index.Name;
-                            if (!exampleSearchValues.ContainsKey(key))
-                                exampleSearchValues.Add(key, 0);
+                        ExtractValuesForSearchParameterFromFile(exampleSearchValues, resource);
 
-                            // Extract the values from the example
-                            var resourceModel = FluentPath.ModelNavigator.CreateInput(resource);
-                            var navigator = FluentPath.ModelNavigator.CreateInput(resource);
-                            var results = Hl7.FluentPath.PathExpression.Select(index.Expression, resourceModel, navigator);
-                            if (results.Count() > 0)
+                        if (resource is Bundle)
+                        {
+                            foreach (var item in (resource as Bundle).Entry)
                             {
-                                foreach (var t2 in results)
+                                if (item.Resource != null)
                                 {
-                                    if (t2 != null)
-                                    {
-                                        if (t2 is FluentPath.ModelNavigator && (t2 as FluentPath.ModelNavigator).FhirValue != null)
-                                        {
-                                            // Validate the type of data returned against the type of search parameter
-                                            Debug.Write(index.Resource + "." + index.Name + ": ");
-                                            Debug.WriteLine((t2 as FluentPath.ModelNavigator).FhirValue.ToString());// + "\r\n";
-                                            exampleSearchValues[key]++;
-                                            // System.Diagnostics.Trace.WriteLine(string.Format("{0}: {1}", xpath.Value, t2.AsStringRepresentation()));
-                                        }
-                                        else
-                                        {
-                                            if (t2.Value is Hl7.FluentPath.ConstantValue)
-                                            {
-                                                Debug.Write(index.Resource + "." + index.Name + ": ");
-                                                Debug.WriteLine((t2.Value as Hl7.FluentPath.ConstantValue).Value);
-                                                exampleSearchValues[key]++;
-                                            }
-                                        }
-                                    }
+                                    ExtractValuesForSearchParameterFromFile(exampleSearchValues, item.Resource);
                                 }
                             }
                         }
@@ -118,8 +90,77 @@ namespace Hl7.Fhir.Tests.Model
                 errorCount++;
             }
 
-            Assert.IsTrue(235 >= errorCount, String.Format("Failed Validating, missing data in {0} of {1} search parameters", missingSearchValues.Count(), exampleSearchValues.Count));
+            Assert.IsTrue(220 >= errorCount, String.Format("Failed Validating, missing data in {0} of {1} search parameters", missingSearchValues.Count(), exampleSearchValues.Count));
         }
 
+        private static void ExtractValuesForSearchParameterFromFile(Dictionary<string, int> exampleSearchValues, Resource resource)
+        {
+            // Extract the search properties
+            var searchparameters = ModelInfo.SearchParameters.Where(r => r.Resource == resource.ResourceType.ToString() && !String.IsNullOrEmpty(r.Expression));
+            foreach (var index in searchparameters)
+            {
+                // prepare the search data cache
+                string key = resource.ResourceType.ToString() + "_" + index.Name;
+                if (!exampleSearchValues.ContainsKey(key))
+                    exampleSearchValues.Add(key, 0);
+
+                // Extract the values from the example
+                ExtractExamplesFromResource(exampleSearchValues, resource, index, key);
+            }
+
+            // If there are any contained resources, extract index data from those too!
+            if (resource is DomainResource)
+            {
+                if ((resource as DomainResource).Contained != null && (resource as DomainResource).Contained.Count > 0)
+                {
+                    foreach (var conResource in (resource as DomainResource).Contained)
+                    {
+                        ExtractValuesForSearchParameterFromFile(exampleSearchValues, conResource);
+                    }
+                }
+            }
+        }
+
+        private static void ExtractExamplesFromResource(Dictionary<string, int> exampleSearchValues, Resource resource, ModelInfo.SearchParamDefinition index, string key)
+        {
+            var resourceModel = FluentPath.ModelNavigator.CreateInput(resource);
+            var navigator = FluentPath.ModelNavigator.CreateInput(resource);
+            var results = Hl7.FluentPath.PathExpression.Select(index.Expression, resourceModel, navigator);
+            if (results.Count() > 0)
+            {
+                foreach (var t2 in results)
+                {
+                    if (t2 != null)
+                    {
+                        if (t2 is FluentPath.ModelNavigator && (t2 as FluentPath.ModelNavigator).FhirValue != null)
+                        {
+                            // Validate the type of data returned against the type of search parameter
+                       //     Debug.Write(index.Resource + "." + index.Name + ": ");
+                       //     Debug.WriteLine((t2 as FluentPath.ModelNavigator).FhirValue.ToString());// + "\r\n";
+                            exampleSearchValues[key]++;
+                            // System.Diagnostics.Trace.WriteLine(string.Format("{0}: {1}", xpath.Value, t2.AsStringRepresentation()));
+                        }
+                        else if (t2.Value is Hl7.FluentPath.ConstantValue)
+                        {
+                       //     Debug.Write(index.Resource + "." + index.Name + ": ");
+                       //     Debug.WriteLine((t2.Value as Hl7.FluentPath.ConstantValue).Value);
+                            exampleSearchValues[key]++;
+                        }
+                        else if (t2.Value is bool)
+                        {
+                       //     Debug.Write(index.Resource + "." + index.Name + ": ");
+                       //     Debug.WriteLine((bool)t2.Value);
+                            exampleSearchValues[key]++;
+                        }
+                        else
+                        {
+                            Debug.Write(index.Resource + "." + index.Name + ": ");
+                            Debug.WriteLine(t2.Value);
+                            exampleSearchValues[key]++;
+                        }
+                    }
+                }
+            }
+        }
     }
 }
