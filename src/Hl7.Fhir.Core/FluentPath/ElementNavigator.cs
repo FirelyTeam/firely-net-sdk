b2b38d4c4c103e41b41c763e5835b346f8a39a57
Brian Postlethwaite <brianpos@optushome.com.au>
2016-08-24 00:33:22 +1000

    Fix issue with the Fhir Integer being Int32 and fluentpath expecting it to be a long.

--- src/Hl7.Fhir.Core/FluentPath/ElementNavigator.cs
+++ src/Hl7.Fhir.Core/FluentPath/ElementNavigator.cs
@@ -84,7 +84,8 @@ namespace Hl7.Fhir.FluentPath
         {
             get
             {
-                if (_string != null) return _string;
+                if (_string != null)
+                    return _string;
 
                 if (_pocoElement is FhirDateTime)
                     return Hl7.FluentPath.PartialDateTime.FromDateTime(((FhirDateTime)_pocoElement).ToDateTimeOffset());
@@ -92,6 +93,12 @@ namespace Hl7.Fhir.FluentPath
                     return Hl7.FluentPath.Time.Parse(((Hl7.Fhir.Model.Time)_pocoElement).Value);
                 else if ((_pocoElement is Hl7.Fhir.Model.Date))
                     return Hl7.FluentPath.PartialDateTime.Parse(((Hl7.Fhir.Model.Date)_pocoElement).Value);
+                else if ((_pocoElement is Integer))
+                {
+                    if ((_pocoElement as Integer).Value.HasValue)
+                        return (long)(_pocoElement as Integer).Value.Value;
+                    return null;
+                }
                 else if (_pocoElement is Hl7.Fhir.Model.Instant)
                 {
                     if (!((Hl7.Fhir.Model.Instant)_pocoElement).Value.HasValue)
