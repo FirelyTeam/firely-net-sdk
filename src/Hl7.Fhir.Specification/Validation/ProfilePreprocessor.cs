7013a5ed6f5ee0cc4149595d294f64711540b02b
Ewout Kramer <e.kramer@furore.com>
2016-10-25 13:32:46 +0200

    feature: Added validation of extensions based on their url

--- src/Hl7.Fhir.Specification/Validation/ProfilePreprocessor.cs
+++ src/Hl7.Fhir.Specification/Validation/ProfilePreprocessor.cs
@@ -39,6 +39,13 @@ namespace Hl7.Fhir.Validation
             // This is only for resources, but I don't bother checking, since this will return empty anyway
             _profiles.AddStatedProfile(instance.GetChildrenByName("meta").ChildrenValues("profile").Cast<string>());
                      
+            // Almost identically, extensions can declare adherance to a profile using the 'url' attribute
+            if(declaredTypeProfile == ModelInfo.CanonicalUriForFhirCoreType(FHIRDefinedType.Extension))
+            {
+                var urlDeclaration = instance.GetChildrenByName("url").FirstOrDefault()?.Value as string;
+                if (urlDeclaration != null) _profiles.AddStatedProfile(urlDeclaration);
+            }
+
             if(additionalProfiles != null) _profiles.AddStatedProfile(additionalProfiles);
             if(additionalCanonicals != null) _profiles.AddStatedProfile(additionalCanonicals);
         }
