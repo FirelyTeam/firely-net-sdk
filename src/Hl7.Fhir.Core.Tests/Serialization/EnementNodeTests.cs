66eca673c1d2d9e5ec2586d2d42d3a7c4a34ca29
Martijn Harthoorn <m.harthoorn@furore.com>
2016-11-07 14:57:51 +0100

    add: ElementNodeWriter

---
+++ src/Hl7.Fhir.Core.Tests/Serialization/EnementNodeTests.cs
@@ -0,0 +1,48 @@
+﻿using Hl7.ElementModel;
+using Hl7.Fhir.FluentPath;
+using Hl7.Fhir.Model;
+using Hl7.FluentPath;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Hl7.Fhir.Serialization
+{
+    [TestClass]
+    public class EnementNodeTests
+    {
+
+        [TestMethod]
+        public void TestCreateFromNavigator()
+        {
+            var parser = new FhirJsonParser();
+            ElementNode node;
+            string json = File.ReadAllText(@"TestData\TestPatient.json");
+            var patient = parser.Parse<Patient>(json);
+
+            var nav = new PocoNavigator(patient);
+            var root = ElementNodeWriter.CreateElementModel(nav);
+
+            // Root
+            Assert.AreEqual("Patient", root.Name);
+
+            // Patient.text.Status
+            node = root.Children[1].Children[0];
+            Assert.AreEqual("status", node.Name);
+            Assert.AreEqual("generated", node.Value);
+            Assert.AreEqual("code", node.TypeName);
+
+            // Patient.address.period.start -- mind the arrays when counting!
+            node = root.Children[11].Children[7].Children[0];
+            Assert.AreEqual("start", node.Name);
+            Assert.AreEqual("dateTime", node.TypeName);
+            Assert.IsInstanceOfType(node.Value, typeof(PartialDateTime));
+            Assert.AreEqual(PartialDateTime.Parse("1974-12-25"), node.Value);
+
+        }
+    }
+}
---
+++ src/Hl7.Fhir.Core/FluentPath/ElementNodeWriter.cs
@@ -0,0 +1,46 @@
+﻿using Hl7.ElementModel;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Hl7.Fhir.Serialization 
+{
+    public static class ElementNodeWriter
+    {
+        public static ElementNode CreateElementModel(this IElementNavigator navigator)
+        {
+            var children = CreateChildren(navigator.Clone());
+            
+            // this is trickery to deal with the limited constructor
+            var root = ElementNode.Valued(navigator.Name, navigator.Value, navigator.TypeName, children.ToArray());
+            return root;
+        }
+     
+        private static IList<ElementNode> CreateChildren(this IElementNavigator navigator)
+        {
+            var nodes = new List<ElementNode>();
+
+            if (navigator.MoveToFirstChild())
+            {
+                do
+                {
+                    var children = CreateChildren(navigator.Clone());
+
+                    // todo: this is trickery to comply to the limited constructor
+                    ElementNode node = ElementNode.Valued(navigator.Name, navigator.Value, navigator.TypeName, children.ToArray());
+                    nodes.Add(node);
+                }
+                while (navigator.MoveToNext());
+            }
+
+            return nodes;
+        }
+
+        public static ElementNode CreateElementNode(this IElementNavigator navigator)
+        {
+            return ElementNode.Valued(navigator.Name, navigator.Value, navigator.TypeName);
+        }
+    }
+}
