c5e38d14f0f7a647a20a03969cbb526b064d94bf
Brian Postlethwaite <brian_pos@hotmail.com>
2017-04-05 11:31:23 +1000

    The debuggerdisplay for the elementdefinition display is more usedful to just show the element in focus, not the list of the whole 53 elements with a tiny * somewhere in there that is difficult to locate.

--- src/Hl7.Fhir.Specification/Specification/Navigation/ElementDefinitionNavigator.cs
+++ src/Hl7.Fhir.Specification/Specification/Navigation/ElementDefinitionNavigator.cs
@@ -547,10 +547,11 @@ namespace Hl7.Fhir.Specification.Navigation
             get
             {
                 var output = new StringBuilder();
-                foreach (var elem in Elements)
-                {
-                    output.AppendFormat("{0}{1}\r\n", elem == Current ? "*" : "", elem.Path);
-                }
+                output.Append(Current.Path);
+                //foreach (var elem in Elements)
+                //{
+                //    output.AppendFormat("{0}{1}\r\n", elem == Current ? "*" : "", elem.Path);
+                //}
                 return output.ToString();
             }
         }
