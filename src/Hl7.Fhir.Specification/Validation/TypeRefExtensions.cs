d7ffab5d40c533a1954064d1bd858841c3e8f48f
Brian Postlethwaite <brian_pos@hotmail.com>
2017-04-05 14:04:26 +1000

    Update for STU3

--- src/Hl7.Fhir.Specification/Validation/TypeRefExtensions.cs
+++ src/Hl7.Fhir.Specification/Validation/TypeRefExtensions.cs
@@ -31,9 +31,18 @@ namespace Hl7.Fhir.Validation
 
         public static string GetDeclaredProfiles(this ElementDefinition.TypeRefComponent typeRef)
         {
+            if (typeRef.Code == FHIRAllTypes.Reference.GetLiteral())
+            {
+                if (!System.String.IsNullOrEmpty(typeRef.TargetProfile))
+                {
+                    return typeRef.TargetProfile;
+                }
+                // It needs to be a reference to a resource.
+                return ModelInfo.CanonicalUriForFhirCoreType(FHIRAllTypes.Resource.GetLiteral());
+            }
             if (!System.String.IsNullOrEmpty(typeRef.Profile))
             {
-                return typeRef.Profile;     // Take the first, this will disappear in STU3 anyway
+                return typeRef.Profile;
             }
             else if (!string.IsNullOrEmpty(typeRef.Code))
                 return ModelInfo.CanonicalUriForFhirCoreType(typeRef.Code);
