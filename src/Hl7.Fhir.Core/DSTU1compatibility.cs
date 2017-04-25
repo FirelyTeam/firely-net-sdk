5e31f7ee0d96bacda23d49ae70a9eea4de79f592
Brian Postlethwaite <brian_pos@hotmail.com>
2017-04-26 06:12:36 +1000

    Include some <summary> tags to remove warnings

--- src/Hl7.Fhir.Core/DSTU1compatibility.cs
+++ src/Hl7.Fhir.Core/DSTU1compatibility.cs
@@ -14,18 +14,30 @@ namespace Hl7.Fhir.Model
     {
     }
 
+    /// <summary>
+    /// The ResourceEntry class has been made obsolete, please change to the class Bundle.EntryComponent instead
+    /// </summary>
     [Obsolete("The ResourceEntry class has been made obsolete, please change to the class Bundle.EntryComponent instead", true)]
     public class ResourceEntry : Bundle.EntryComponent
     {
     }
 
-
+    /// <summary>
+    /// The ResourceEntry class has been made obsolete, please change to the class Bundle.EntryComponent instead
+    /// </summary>
+    /// <typeparam name="T"></typeparam>
     [Obsolete("The ResourceEntry class has been made obsolete, please change to the class Bundle.EntryComponent instead", true)]
     public class ResourceEntry<T> : Bundle.EntryComponent where T : Resource
     {
+        /// <summary>
+        /// The ResourceEntry class has been made obsolete, please change to the class Bundle.EntryComponent instead
+        /// </summary>
         public new T Resource { get; set; }
     }
 
+    /// <summary>
+    /// The 'Alert' resource was renamed to 'Flag' in DSTU2
+    /// </summary>
     [Obsolete("The 'Alert' resource was renamed to 'Flag' in DSTU2", true)]
     public partial class Alert : Flag
     {
@@ -33,15 +45,24 @@ namespace Hl7.Fhir.Model
 
     public partial class Flag
     {
+        /// <summary>
+        /// This property was renamed to 'Code' in DSTU2, and re-typed from string to CodeableConcept. When using un-coded values, just populate the text property of the codeableconcept
+        /// </summary>
         [Obsolete("This property was renamed to 'Code' in DSTU2, and re-typed from string to CodeableConcept. When using un-coded values, just populate the text property of the codeableconcept", true)]
         public CodeableConcept Note { get; set; }
     }
 
     public partial class Observation
     {
+        /// <summary>
+        /// This property was renamed to 'Code' in DSTU2
+        /// </summary>
         [Obsolete("This property was renamed to 'Code' in DSTU2", true)]
         public CodeableConcept Name { get; set; }
 
+        /// <summary>
+        /// This property was renamed to 'Effective' in DSTU2
+        /// </summary>
         [Obsolete("This property was renamed to 'Effective' in DSTU2", true)]
         public FhirDateTime Applies { get; set; }
     }
