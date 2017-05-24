9d9300bca1a7d2dabcdf08c777f4cbd9ece08764
Brian Postlethwaite <brian_pos@hotmail.com>
2017-05-24 22:19:11 +1000

    Refactor the Spark.Engine project into a simple WebApi assembly that doesn't have any storage dependencies

---
+++ src/Hl7.Fhir.WebApi/BinaryFormatter.cs
@@ -0,0 +1,98 @@
+﻿/* 
+ * Copyright (c) 2017+ brianpos, Furore and contributors
+ * See the file CONTRIBUTORS for details.
+ * 
+ * This file is licensed under the BSD 3-Clause license
+ * available at https://github.com/ewoutkramer/fhir-net-api/blob/master/LICENSE
+ */
+
+using Hl7.Fhir.Model;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net.Http;
+using System.Net.Http.Formatting;
+using System.Net.Http.Headers;
+using System.Threading.Tasks;
+using System.Web;
+
+namespace Hl7.Fhir.WebApi
+{
+    public class BinaryFhirFormatter : FhirMediaTypeFormatter
+    {
+        public BinaryFhirFormatter() : base()
+        {
+            SupportedMediaTypes.Add(new MediaTypeHeaderValue(FhirMediaType.BinaryResource));
+        }
+
+        public override bool CanReadType(Type type)
+        {
+            if (typeof(Resource).IsAssignableFrom(type) || type == typeof(OperationOutcome))
+                return true;
+            return false;
+        }
+
+        public override bool CanWriteType(Type type)
+        {
+            if (typeof(Resource).IsAssignableFrom(type) || type == typeof(OperationOutcome))
+                return true;
+            return false;
+        }
+
+        public override Task<object> ReadFromStreamAsync(Type type, Stream readStream, HttpContent content, IFormatterLogger formatterLogger)
+        {
+            MemoryStream stream = new MemoryStream();
+            readStream.CopyTo(stream);
+
+            IEnumerable<string> xContentHeader;
+            var success = content.Headers.TryGetValues("X-Content-Type", out xContentHeader);
+
+            if (!success)
+            {
+                throw new HttpException((int)System.Net.HttpStatusCode.BadRequest, "POST to binary must provide a Content-Type header");
+            }
+
+            string contentType = xContentHeader.FirstOrDefault();
+
+            Binary binary = new Binary();
+            binary.Content = stream.ToArray();
+            binary.ContentType = contentType;
+
+            return System.Threading.Tasks.Task.FromResult<object>(binary);
+        }
+
+        private string GetFileExtensionForMimeType(string mimetype)
+        {
+            Microsoft.Win32.RegistryKey regKey = Microsoft.Win32.Registry.ClassesRoot.OpenSubKey(@"MIME\Database\Content Type\" + mimetype, false);
+            if (regKey != null && regKey.GetValue("Extension") != null)
+                return regKey.GetValue("Extension").ToString();
+            return null;
+        }
+
+        public override System.Threading.Tasks.Task WriteToStreamAsync(Type type, object value, Stream writeStream, HttpContent content, System.Net.TransportContext transportContext)
+        {
+            if (type == typeof(Binary))
+            {
+                Binary binary = (Binary)value;
+
+                content.Headers.ContentType = new MediaTypeHeaderValue(binary.ContentType);
+                content.Headers.ContentDisposition = new ContentDispositionHeaderValue("attachment")
+                {
+                    FileName = String.Format("fhir_binary_{0}_{1}.{2}",
+                    binary.Id,
+                    binary.Meta != null ? binary.Meta.VersionId : "0",
+                    GetFileExtensionForMimeType(binary.ContentType))
+                };
+
+                var stream = new MemoryStream(binary.Content);
+                stream.CopyTo(writeStream);
+                stream.Flush();
+            }
+            if (type == typeof(OperationOutcome))
+            {
+            }
+            return System.Threading.Tasks.Task.CompletedTask;
+        }
+    }
+}
---
+++ src/Hl7.Fhir.WebApi/FhirMediaTypeFormatter.cs
@@ -0,0 +1,86 @@
+﻿/* 
+ * Copyright (c) 2017+ brianpos, Furore and contributors
+ * See the file CONTRIBUTORS for details.
+ * 
+ * This file is licensed under the BSD 3-Clause license
+ * available at https://github.com/ewoutkramer/fhir-net-api/blob/master/LICENSE
+ */
+
+using Hl7.Fhir.Model;
+using System;
+using System.Net.Http;
+using System.Net.Http.Formatting;
+using System.Net.Http.Headers;
+using System.Text;
+using System.IO;
+using System.Net;
+
+namespace Hl7.Fhir.WebApi
+{
+    public abstract class FhirMediaTypeFormatter : MediaTypeFormatter
+    {
+        public FhirMediaTypeFormatter() : base()
+        {
+            this.SupportedEncodings.Clear();
+            this.SupportedEncodings.Add(Encoding.UTF8);
+        }
+
+        protected Resource entry = null;
+
+        private void setEntryHeaders(HttpContentHeaders headers)
+        {
+            if (entry != null)
+            {
+                if (entry.Meta != null && entry.Meta.LastUpdated.HasValue)
+                    headers.LastModified = entry.Meta.LastUpdated.Value.UtcDateTime;
+                else
+                    headers.LastModified = DateTimeOffset.UtcNow;
+                if (!string.IsNullOrEmpty(entry.Id))
+                    headers.ContentLocation = new Uri(entry.ResourceIdentity(entry.ResourceBase).OriginalString);
+
+                if (entry is Binary)
+                {
+                    headers.ContentType = new MediaTypeHeaderValue( ((Binary)entry).ContentType);
+                }
+            }
+        }
+
+        public override bool CanReadType(Type type)
+        {
+            if (typeof(Resource).IsAssignableFrom(type))
+                return true;
+            return false;
+        }
+
+        public override bool CanWriteType(Type type)
+        {
+            if (typeof(Resource).IsAssignableFrom(type) || type == typeof(OperationOutcome))
+                return true;
+            return false;
+        }
+
+        public override void SetDefaultContentHeaders(Type type, HttpContentHeaders headers, MediaTypeHeaderValue mediaType)
+        {
+            base.SetDefaultContentHeaders(type, headers, mediaType);
+            setEntryHeaders(headers);
+        }
+
+        public override MediaTypeFormatter GetPerRequestFormatterInstance(Type type, HttpRequestMessage request, MediaTypeHeaderValue mediaType)
+        {
+            this.entry = request.GetEntry();
+            return base.GetPerRequestFormatterInstance(type, request, mediaType);
+        }
+
+        protected string ReadBodyFromStream(Stream readStream, HttpContent content)
+        {
+            var charset = content.Headers.ContentType.CharSet ?? Encoding.UTF8.HeaderName;
+            var encoding = Encoding.GetEncoding(charset);
+
+            if (encoding != Encoding.UTF8)
+                throw new FhirServerException(HttpStatusCode.BadRequest, "FHIR supports UTF-8 encoding exclusively, not " + encoding.WebName);
+
+            StreamReader sr = new StreamReader(readStream, Encoding.UTF8, true);
+            return sr.ReadToEnd();
+        }
+    }
+}
