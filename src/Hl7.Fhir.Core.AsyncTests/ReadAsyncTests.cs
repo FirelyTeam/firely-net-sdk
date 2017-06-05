7f68d82d15164def609ca5353f592259e77df4cc
Ricardo Pineda <ricardopinedathen@gmail.com>
2017-06-04 20:30:31 -0400

    Add async methods to the FHIR Client while keeping sync methods for backward compatibility
    - Update BundleExtensions with new async method
    - Update FhirClient,FhirClientOperations,FhirClientSearch,Requester with new async methods, preivous sync methods retained
    - Add TaskExtensions to allow easier task handling for old sync methods
    - Add integration test project for async methods

---
+++ src/Hl7.Fhir.Core.AsyncTests/ReadAsyncTests.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Linq;
+using System.Threading.Tasks;
+using FluentAssertions;
+using Hl7.Fhir.Model;
+using Hl7.Fhir.Rest;
+using NUnit.Framework;
+
+namespace Hl7.Fhir.Core.AsyncTests
+{
+    [TestFixture]
+    public class ReadAsyncTests
+    {
+        private string _endpoint = "https://api.hspconsortium.org/rpineda/open";
+        [Test]
+        public async Task Read_UsingResourceIdentity_ResultReturned()
+        {
+            var client = new FhirClient(_endpoint);
+            client.PreferredFormat = ResourceFormat.Json;
+            client.ReturnFullResource = true;
+
+            Patient p = await client.ReadAsync<Patient>(new ResourceIdentity("/Patient/SMART-1288992"));
+            p.Should().NotBeNull();
+            p.Name[0].Given.Should().NotBeNull();
+            p.Name[0].Family.Should().NotBeNull();
+            Console.WriteLine($"NAME: {p.Name[0].Given.FirstOrDefault()} {p.Name[0].Family.FirstOrDefault()}");
+            Console.WriteLine("Test Completed");
+        }
+        [Test]
+        public async Task Read_UsingLocationString_ResultReturned()
+        {
+            var client = new FhirClient(_endpoint);
+            client.PreferredFormat = ResourceFormat.Json;
+            client.ReturnFullResource = true;
+
+            Patient p = await client.ReadAsync<Patient>("/Patient/SMART-1288992");
+            p.Should().NotBeNull();
+            p.Name[0].Given.Should().NotBeNull();
+            p.Name[0].Family.Should().NotBeNull();
+            Console.WriteLine($"NAME: {p.Name[0].Given.FirstOrDefault()} {p.Name[0].Family.FirstOrDefault()}");
+            Console.WriteLine("Test Completed");
+        }
+    }
+}
