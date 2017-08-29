b548f0e9381ff06ba0c55d1c780a393f66f2b483
Ewout Kramer <e.kramer@furore.com>
2017-08-29 14:35:41 +0200

    fix: remove incorrect using statement

--- src/Hl7.Fhir.Specification.Tests/Snapshot/SnapshotGeneratorTest.cs
+++ src/Hl7.Fhir.Specification.Tests/Snapshot/SnapshotGeneratorTest.cs
@@ -22,7 +22,6 @@ using Hl7.Fhir.Rest;
 using System.Text;
 using System.Xml;
 using Hl7.Fhir.Utility;
-using static Hl7.Fhir.Model.ElementDefinition.DiscriminatorComponent;
 
 namespace Hl7.Fhir.Specification.Tests
 {
