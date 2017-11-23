8d6a2d8600b69ac77fe0c0adfdf71dbaa73506e3
Marco Visser <m.visser@furore.com>
2017-11-23 11:33:07 +0100

    fix: startdate can be "0001-01-01", issue #474

---
+++ src/Hl7.Fhir.Support.Tests/Model/PartialDateTimeTest.cs
@@ -0,0 +1,65 @@
+﻿using Hl7.Fhir.Model.Primitives;
+using System.Collections.Generic;
+using Xunit;
+
+namespace Hl7.Fhir.Support.Tests.Model
+{
+    public class PartialDateTimeTest
+    {
+        [Theory]
+        [MemberData(nameof(GetTestdata))]
+        public void OperatorsTest(string leftOperand, string operation, string rightOperand, bool expectedResult)
+        {
+            PartialDateTime l, r;
+
+            l = PartialDateTime.Parse(leftOperand);
+            r = PartialDateTime.Parse(rightOperand);
+
+            bool? result = null;
+
+            switch (operation)
+            {
+                case ">":
+                    result = l > r;
+                    break;
+                case ">=":
+                    result = l >= r;
+                    break;
+                case "<":
+                    result = l < r;
+                    break;
+                case "<=":
+                    result = l <= r;
+                    break;
+                case "==":
+                    result = l == r;
+                    break;
+                case "!=":
+                    result = l != r;
+                    break;
+
+                default:
+                    break;
+            }
+
+            Assert.NotNull(result);
+            Assert.Equal(expectedResult, result);
+        }
+
+        private static IEnumerable<object[]> GetTestdata()
+        {
+            return new List<object[]>()
+            {
+                new object[]{ "0002", ">", "0001",  true },
+                new object[]{ "0001", ">", "0001",  false },
+                new object[]{ "0001", ">=", "0001",  true},
+                new object[]{ "0001", ">", "0001",  false },
+                new object[]{ "2017-11-21T16:03:12", ">", "2017-11-21T16:03:11",  true },
+                new object[]{ "2017-11-21T16:03:12+01:00", ">", "2017-11-21T17:03:11+02:00",  true },
+                new object[]{ "2017-11-21T16:03:11+01:00", "==", "2017-11-21T17:03:11+02:00",  true },
+                new object[]{ "2018-01-01", "==", "2018-01-01",  true },
+                new object[]{ "2018-01", "<", "2018-02",  true },
+            };
+        }
+    }
+}
---
+++ src/Hl7.Fhir.Support.Tests/Serialization/PrimitiveTypeConverterTest.cs
@@ -0,0 +1,48 @@
+﻿using Hl7.Fhir.Serialization;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace Hl7.Fhir.Support.Tests.Serialization
+{
+    public class PrimitiveTypeConverterTest
+    {
+        [Theory]
+        [MemberData(nameof(GetTestdata))]
+        public void ConvertToDateTimeOffsetTest(string input, DateTimeOffset expected, bool expectException)
+        {
+            try
+            {
+                var result = PrimitiveTypeConverter.ConvertTo<DateTimeOffset>(input);
+                Assert.NotNull(result);
+                Assert.Equal(expected, result);
+
+            }
+            catch (Exception)
+            {
+                if (!expectException) Assert.True(false);
+            }
+        }
+
+        public static IEnumerable<object[]> GetTestdata()
+        {
+            return new List<object[]>()
+            {
+                new object[]{ "0001", new DateTimeOffset(1,1,1,0,0,0,new TimeSpan()), false },
+                new object[]{ "nodate", null, true },
+                new object[]{ "2018", new DateTimeOffset(2018,1,1,0,0,0,new TimeSpan()), false },
+                new object[]{ "0001-01", new DateTimeOffset(1,1,1,0,0,0,new TimeSpan()), false },
+                new object[]{ "0001-01-01", new DateTimeOffset(1,1,1,0,0,0,new TimeSpan()), false },
+                new object[]{ "0001-01-32", null, true},
+                new object[]{ "0001-01-02", new DateTimeOffset(1,1,2,0,0,0,new TimeSpan()), false },
+                new object[]{ "0001-01-02T00:00:00", new DateTimeOffset(new DateTime(1,1,2,0,0,0)), false }, // use localTime 
+                new object[]{ "0001-01-02T00:00:00Z", new DateTimeOffset(1,1,2,0,0,0,new TimeSpan()), true },
+                new object[]{ "0001-01-01T00:00:00+01:00", null, true },
+            };
+        }
+
+    }
+}
