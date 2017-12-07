43ee715a10e258e39cf5ddbcdb31083d9aa4441b
Michele Mottini <michele.mottini@gmail.com>
2017-12-06 19:37:15 -0500

    Core: conversion of version-independent OperationOutcomeIssue to/from version-specific OperationOutcome.IssueComponent

--- src/Hl7.Fhir.Core/Model/OperationOutcomeIssue.cs
+++ src/Hl7.Fhir.Core/Model/OperationOutcomeIssue.cs
@@ -5,9 +5,50 @@
     /// </summary>
     public class OperationOutcomeIssue
     {
+        public OperationOutcomeIssue()
+        { }
+
+        public OperationOutcomeIssue( DSTU2.OperationOutcome.IssueComponent issue )
+        {
+            Code = issue?.Code;
+            Severity = issue?.Severity;
+            Details = (CodeableConcept)issue?.Details.DeepCopy();
+            Diagnostics = issue.Diagnostics;
+        }
+
+        public OperationOutcomeIssue(STU3.OperationOutcome.IssueComponent issue)
+        {
+            Code = issue?.Code;
+            Severity = issue?.Severity;
+            Details = (CodeableConcept)issue?.Details.DeepCopy();
+            Diagnostics = issue.Diagnostics;
+        }
+
         public IssueType? Code { get; set; }
         public IssueSeverity? Severity { get; set; }
         public CodeableConcept Details { get; set; }
         public string Diagnostics { get; set; }
+
+        public DSTU2.OperationOutcome.IssueComponent ToDstu2()
+        {
+            return new DSTU2.OperationOutcome.IssueComponent
+            {
+                Code = Code,
+                Severity = Severity,
+                Details = (CodeableConcept)Details.DeepCopy(),
+                Diagnostics = Diagnostics
+            };
+        }
+
+        public STU3.OperationOutcome.IssueComponent ToStu3()
+        {
+            return new STU3.OperationOutcome.IssueComponent
+            {
+                Code = Code,
+                Severity = Severity,
+                Details = (CodeableConcept)Details.DeepCopy(),
+                Diagnostics = Diagnostics
+            };
+        }
     }
 }
