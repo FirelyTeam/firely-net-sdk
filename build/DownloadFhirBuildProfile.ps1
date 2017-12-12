3ac889b26856be757c6e83fb16f2aae71651e8ed
Michele Mottini <michele.mottini@gmail.com>
2017-12-12 09:55:40 -0500

    Build: download DSTU2 and STU3 definitions

--- build/DownloadFhirBuildProfile.ps1
+++ build/DownloadFhirBuildProfile.ps1
@@ -5,7 +5,7 @@ cls
 
 # Script to be run from 'build' directory
 
-$server = "http://hl7-fhir.github.io";
+$server = "http://hl7.org/fhir";
 $baseDir = Resolve-Path ..
 $srcdir = "$baseDir\src";
 
@@ -13,6 +13,7 @@ $srcdir = "$baseDir\src";
 # This also has some basic capabilities to go through proxies without any additional configuration.
 function PowerCurl($targetPath, $sourceUrl)
 {
+	Write-Host "Downloading $sourceUrl to $targetPath"
 	Try
 	{
         $webclient = New-Object System.Net.WebClient
@@ -25,18 +26,24 @@ function PowerCurl($targetPath, $sourceUrl)
 }
 
 
-# Download the core profiles for code generation
-PowerCurl "$srcdir\Hl7.Fhir.Core\Model\Source\expansions.xml" "$server/expansions.xml"
-PowerCurl "$srcdir\Hl7.Fhir.Core\Model\Source\profiles-resources.xml" "$server/profiles-resources.xml"
-PowerCurl "$srcdir\Hl7.Fhir.Core\Model\Source\profiles-types.xml" "$server/profiles-types.xml"
-PowerCurl "$srcdir\Hl7.Fhir.Core\Model\Source\search-parameters.xml" "$server/search-parameters.xml"
+# Download the DSTU2 core profiles for code generation
+PowerCurl "$srcdir\Hl7.Fhir.Core\Model\Source-DSTU2\expansions.xml" "$server/DSTU2/expansions.xml"
+PowerCurl "$srcdir\Hl7.Fhir.Core\Model\Source-DSTU2\profiles-resources.xml" "$server/DSTU2/profiles-resources.xml"
+PowerCurl "$srcdir\Hl7.Fhir.Core\Model\Source-DSTU2\profiles-types.xml" "$server/DSTU2/profiles-types.xml"
+PowerCurl "$srcdir\Hl7.Fhir.Core\Model\Source-DSTU2\search-parameters.xml" "$server/DSTU2/search-parameters.xml"
 
-PowerCurl "$srcdir\Hl7.Fhir.Core.Tests\TestData\careplan-example-f201-renal.xml" "$server/careplan-example-f201-renal.xml"
-PowerCurl "$srcdir\Hl7.Fhir.Core.Tests\TestData\examples.zip" "$server/examples.zip"
-PowerCurl "$srcdir\Hl7.Fhir.Core.Tests\TestData\examples-json.zip" "$server/examples-json.zip"
-PowerCurl "$srcdir\Hl7.Fhir.Core.Tests\TestData\json-edge-cases.json" "$server/json-edge-cases.json"
+# Download the STU3 core profiles for code generation
+PowerCurl "$srcdir\Hl7.Fhir.Core\Model\Source-STU3\expansions.xml" "$server/STU3/expansions.xml"
+PowerCurl "$srcdir\Hl7.Fhir.Core\Model\Source-STU3\profiles-resources.xml" "$server/STU3/profiles-resources.xml"
+PowerCurl "$srcdir\Hl7.Fhir.Core\Model\Source-STU3\profiles-types.xml" "$server/STU3/profiles-types.xml"
+PowerCurl "$srcdir\Hl7.Fhir.Core\Model\Source-STU3\search-parameters.xml" "$server/STU3/search-parameters.xml"
 
-PowerCurl "$srcdir\Hl7.Fhir.Specification\validation.xml.zip" "$server/validation.xml.zip"
-PowerCurl "$srcdir\Hl7.Fhir.Specification.Tests\TestData\snapshot-test\profiles-others.xml" "$server/profiles-others.xml"
-copy "$srcdir\Hl7.Fhir.Core\Model\Source\profiles-resources.xml" "$srcdir\Hl7.Fhir.Specification.Tests\TestData\snapshot-test"
-copy "$srcdir\Hl7.Fhir.Core\Model\Source\profiles-types.xml" "$srcdir\Hl7.Fhir.Specification.Tests\TestData\snapshot-test"
+PowerCurl "$srcdir\Hl7.Fhir.Core.Tests\TestData\careplan-example-f201-renal.xml" "$server/DSTU2/careplan-example-f201-renal.xml"
+PowerCurl "$srcdir\Hl7.Fhir.Core.Tests\TestData\examples.zip" "$server/DSTU2/examples.zip"
+PowerCurl "$srcdir\Hl7.Fhir.Core.Tests\TestData\examples-json.zip" "$server/DSTU2/examples-json.zip"
+PowerCurl "$srcdir\Hl7.Fhir.Core.Tests\TestData\json-edge-cases.json" "$server/DSTU2/json-edge-cases.json"
+
+PowerCurl "$srcdir\Hl7.Fhir.Specification\validation.xml.zip" "$server/DSTU2/validation.xml.zip"
+PowerCurl "$srcdir\Hl7.Fhir.Specification.Tests\TestData\snapshot-test\profiles-others.xml" "$server/DSTU2/profiles-others.xml"
+copy "$srcdir\Hl7.Fhir.Core\Model\Source-DSTU2\profiles-resources.xml" "$srcdir\Hl7.Fhir.Specification.Tests\TestData\snapshot-test"
+copy "$srcdir\Hl7.Fhir.Core\Model\Source-DSTU2\profiles-types.xml" "$srcdir\Hl7.Fhir.Specification.Tests\TestData\snapshot-test"
