aa1a88e5f84ed7dc0eea3601573093df192902fd
Ryan Kelley <ryan.kelley@royaljay.net>
2018-05-01 13:50:39 -0600

    Add encoding to property names on form post

--- src/Hl7.Fhir.Core/Rest/EntryToHttpExtensions.cs
+++ src/Hl7.Fhir.Core/Rest/EntryToHttpExtensions.cs
@@ -134,7 +134,7 @@ namespace Hl7.Fhir.Rest
                 foreach(Parameters.ParameterComponent parameter in ((Parameters)data).Parameter)
                 {
                     if (!string.IsNullOrEmpty(bodyParameters)) bodyParameters += "&";
-                    bodyParameters += $"{parameter.Name}={Uri.EscapeDataString(parameter.Value.ToString())}";
+                    bodyParameters += $"{Uri.EscapeDataString(parameter.Name)}={Uri.EscapeDataString(parameter.Value.ToString())}";
                 }
                 body = Encoding.UTF8.GetBytes(bodyParameters);
                 request.ContentType = "application/x-www-form-urlencoded";
