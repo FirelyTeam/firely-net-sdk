d6130e27e172e72acb1d4eed660b9a84a4cd05b9
Ewout Kramer <e.kramer@furore.com>
2018-09-25 18:41:12 +0200

    fix: compile error introduced by merge.

--- src/Hl7.Fhir.Specification.Tests/Source/ArtifactSummaryTests.cs
+++ src/Hl7.Fhir.Specification.Tests/Source/ArtifactSummaryTests.cs
@@ -80,10 +80,8 @@ namespace Hl7.Fhir.Specification.Tests
             Assert.AreEqual(url, summary.GetConformanceCanonicalUrl());
             Assert.AreEqual("MainBundle Section title codes", summary.GetConformanceName());
             Assert.AreEqual(PublicationStatus.Draft.GetLiteral(), summary.GetConformanceStatus());
-
-            // Valueset properties
-            Assert.IsNotNull(summary.GetValueSetSystem());
         }
+
 
         [TestMethod]
         public void TestExtensionDefinitionSummary()
@@ -91,41 +89,21 @@ namespace Hl7.Fhir.Specification.Tests
             const string path = @"TestData\snapshot-test\extensions\extension-patient-religion.xml";
             const string url = @"http://hl7.org/fhir/StructureDefinition/patient-religion";
             var summary = assertSummary(path);
-
             // Common properties
             Assert.AreEqual(ResourceType.StructureDefinition.GetLiteral(), summary.ResourceTypeName);
             Assert.IsTrue(summary.ResourceType == ResourceType.StructureDefinition);
-
             // Conformance resource properties
             Assert.IsNotNull(summary.GetConformanceCanonicalUrl());
             Assert.AreEqual(url, summary.GetConformanceCanonicalUrl());
             Assert.AreEqual("religion", summary.GetConformanceName());
             Assert.AreEqual(PublicationStatus.Draft.GetLiteral(), summary.GetConformanceStatus());
-        }
-
-        [TestMethod]
-        public void TestExtensionDefinitionSummary()
-        {
-            const string path = @"TestData\snapshot-test\extensions\extension-us-core-religion.xml";
-            const string url = @"http://hl7.org/fhir/StructureDefinition/us-core-religion";
-            var summary = assertSummary(path);
-
-            // Common properties
-            Assert.AreEqual(ResourceType.StructureDefinition.GetLiteral(), summary.ResourceTypeName);
-            Assert.IsTrue(summary.ResourceType == ResourceType.StructureDefinition);
-
-            // Conformance resource properties
-            Assert.IsNotNull(summary.GetConformanceCanonicalUrl());
-            Assert.AreEqual(url, summary.GetConformanceCanonicalUrl());
-            Assert.AreEqual("Patient's professed religious affiliation", summary.GetConformanceName());
-            Assert.AreEqual(ConformanceResourceStatus.Draft.GetLiteral(), summary.GetConformanceStatus());
-
             // StructureDefinition properties
             var context = summary.GetStructureDefinitionContext();
             Assert.IsNotNull(context);
             Assert.AreEqual(1, context.Length);
             Assert.AreEqual("Patient", context[0]);
         }
+
 
         [TestMethod]
         public void TestProfilesTypesJson()
