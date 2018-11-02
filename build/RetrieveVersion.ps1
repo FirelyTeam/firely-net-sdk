d15439cdef62febfc98e701e1b5d0fcb266f4dc8
Marco Visser <m.visser@furore.com>
2018-11-02 11:36:53 +0100

    Trim the variables

--- build/RetrieveVersion.ps1
+++ build/RetrieveVersion.ps1
@@ -4,9 +4,11 @@ $xml = [xml](get-content ..\src\fhir-net-api.props)
 
 #Get the version 
 [string]$version = $xml.Project.PropertyGroup.VersionPrefix
+$version = $version.Trim()
 
 #Get the suffix
 [string]$suffix = $xml.Project.PropertyGroup.VersionSuffix
+$suffix = $suffix.Trim()
 
 #Setting task variable $CurrentVersion (used for VSTS) 
 Write-Host "##vso[task.setvariable variable=CurrentVersion]$version"
