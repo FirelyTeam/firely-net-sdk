3c46b3471c9a0b6bd91a193f0b39968f696dbfb2
Ewout Kramer <e.kramer@furore.com>
2018-12-19 19:08:43 +0100

    fix: fixed all incorrect test files that now had errors because of better parsing

--- src/Hl7.Fhir.Specification.Tests/Source/TerminologyTests.cs
+++ src/Hl7.Fhir.Specification.Tests/Source/TerminologyTests.cs
@@ -202,7 +202,7 @@ namespace Hl7.Fhir.Specification.Tests
         [Fact, Trait("TestCategory", "IntegrationTest")]
         public void ExternalServiceValidateCodeTest()
         {
-            var client = new FhirClient("http://ontoserver.csiro.au/stu3-latest");
+            var client = new FhirClient("https://ontoserver.csiro.au/stu3-latest");
             var svc = new ExternalTerminologyService(client);
 
             // Do common tests for service
@@ -216,7 +216,7 @@ namespace Hl7.Fhir.Specification.Tests
         [Fact, Trait("TestCategory", "IntegrationTest")]
         public void FallbackServiceValidateCodeTest()
         {
-            var client = new FhirClient("http://ontoserver.csiro.au/stu3-latest");
+            var client = new FhirClient("https://ontoserver.csiro.au/stu3-latest");
             var external = new ExternalTerminologyService(client);
             var local = new LocalTerminologyService(_resolver);
             var svc = new FallbackTerminologyService(local, external);
@@ -231,7 +231,7 @@ namespace Hl7.Fhir.Specification.Tests
         [Fact, Trait("TestCategory", "IntegrationTest")]
         public void FallbackServiceValidateCodeTestWithVS()
         {
-            var client = new FhirClient("http://ontoserver.csiro.au/stu3-latest");
+            var client = new FhirClient("https://ontoserver.csiro.au/stu3-latest");
             var service = new ExternalTerminologyService(client);
             var vs = _resolver.FindValueSet("http://hl7.org/fhir/ValueSet/substance-code");
             Assert.NotNull(vs);
