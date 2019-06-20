66df5537e8646079ce1617989ba0d8131d6f8c4a
Ewout Kramer <e.kramer@furore.com>
2019-06-20 18:01:01 +0200

    fix: improved functions for working with primitive typeinfo

--- src/Hl7.FhirPath/FhirPath/TypeInfo.cs
+++ src/Hl7.FhirPath/FhirPath/TypeInfo.cs
@@ -12,7 +12,7 @@ namespace Hl7.FhirPath
         public static readonly TypeInfo String = new TypeInfo("string");
         public static readonly TypeInfo Integer = new TypeInfo("integer");
         public static readonly TypeInfo Decimal = new TypeInfo("decimal");
-        public static readonly TypeInfo DateTime = new TypeInfo("datetime");
+        public static readonly TypeInfo DateTime = new TypeInfo("dateTime");
         public static readonly TypeInfo Time = new TypeInfo("time");
         public static readonly TypeInfo Any = new TypeInfo("any");
         public static readonly TypeInfo Void = new TypeInfo("void");
@@ -33,70 +33,25 @@ namespace Hl7.FhirPath
                 case "datetime": return TypeInfo.DateTime;
                 case "time": return TypeInfo.Time;
                 case "any": return TypeInfo.Any;
+                case "void": return TypeInfo.Void;
                 default:
                     var result = new TypeInfo(typeName);
-                    result.IsBuiltin = true;
                     return result;
             }
         }
 
         public string Name { get; protected set; }
 
-        public bool IsBuiltin { get; private set; }
-
-        public static TypeInfo ForNativeType(Type nativeType)
-        {
-            if (nativeType == typeof(bool))
-                return TypeInfo.Boolean;
-            else if (nativeType == typeof(string))
-                return TypeInfo.String;
-            else if (nativeType == typeof(long))
-                return TypeInfo.Integer;
-            else if (nativeType == typeof(decimal))
-                return TypeInfo.Decimal;
-            else if (nativeType == typeof(PartialDateTime))
-                return TypeInfo.DateTime;
-            else if (nativeType == typeof(PartialTime))
-                return TypeInfo.Time;
-            else if (nativeType == typeof(IEnumerable<ITypedElement>))
-                return TypeInfo.Any;
-            else if (nativeType == typeof(ITypedElement))
-                return TypeInfo.Any;
-            else
-                throw Error.Argument("nativeType", "Native type '{0}' is not mappable to a FhirPath type".FormatWith(nativeType.Name));
-        }
-
-        public bool MapsToNative(Type t)
-        {
-            if (this == TypeInfo.Boolean && t == typeof(bool))
-                return true;
-            else if (this == TypeInfo.String && t == typeof(string))
-                return true;
-            else if (this == TypeInfo.Integer && t == typeof(long))
-                return true;
-            else if (this == TypeInfo.Decimal && t == typeof(decimal))
-                return true;
-            else if (this == TypeInfo.DateTime && t == typeof(PartialDateTime))
-                return true;
-            else if (this == TypeInfo.Time && t == typeof(PartialTime))
-                return true;
-            else if (this == TypeInfo.Any && t == typeof(object))
-                return true;
-            else
-                return false;
-        }
-
         public override string ToString() => Name;
 
         public override bool Equals(object obj) => Equals(obj as TypeInfo);
-        public bool Equals(TypeInfo other) => other != null && 
-            Name == other.Name && IsBuiltin == other.IsBuiltin;
+        public bool Equals(TypeInfo other) => other != null &&
+            Name == other.Name;
 
         public override int GetHashCode()
         {
             var hashCode = -568888154;
             hashCode = hashCode * -1521134295 + EqualityComparer<string>.Default.GetHashCode(Name);
-            hashCode = hashCode * -1521134295 + IsBuiltin.GetHashCode();
             return hashCode;
         }
 
