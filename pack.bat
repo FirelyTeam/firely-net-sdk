2f32d3710d2fe9201e36c4a3445e56d3140da90c
Ewout Kramer <e.kramer@furore.com>
2019-07-30 12:05:06 +0200

    rewrite: more cleanup

--- pack.bat
+++
@@ -1,3 +0,0 @@
-msbuild src\Hl7.Fhir.sln /t:clean /p:configuration=release /v:minimal %*
-msbuild src\Hl7.Fhir.sln /t:restore /p:configuration=release /v:minimal %*
-msbuild src\Hl7.Fhir.sln /t:build,pack /p:configuration=release /v:minimal %*
