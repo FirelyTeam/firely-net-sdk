440c5fa335dc7134247f19a66f1ca3f89ddb4470
Marten Smits <m.smits@furore.com>
2020-02-10 17:19:19 +0100

    added unit test

--- src/Hl7.Fhir.Specification.Tests/Validation/IssuesTests.cs
+++ src/Hl7.Fhir.Specification.Tests/Validation/IssuesTests.cs
@@ -3,6 +3,7 @@ using Hl7.Fhir.Serialization;
 using Hl7.Fhir.Specification.Source;
 using Hl7.Fhir.Validation;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Collections.Generic;
 
 namespace Hl7.Fhir.Specification.Tests
 {
@@ -27,6 +28,49 @@ namespace Hl7.Fhir.Specification.Tests
             var pat = new FhirJsonParser().Parse<Patient>(json);
 
             var report = validator.Validate(pat);
+            Assert.IsTrue(report.Success);
+        }
+
+        [TestMethod]
+        public void Issue1244AppointmentBundle()
+        {           
+            var appointment = new Appointment
+            {
+                IdElement = new Id("f1b652f7-61c5-4036-a897-9935b245b30c"),
+                Status = Appointment.AppointmentStatus.Booked,
+                Start = new System.DateTimeOffset(2019, 08, 03, 08, 00, 00, System.TimeSpan.FromHours(2.00)),
+                End = new System.DateTimeOffset(2019, 08, 03, 08, 30, 00, System.TimeSpan.FromHours(2.00)),
+                Participant = new List<Appointment.ParticipantComponent>
+                {
+                    new Appointment.ParticipantComponent
+                    {
+                        Actor = new ResourceReference("Practitioner/d33a6cd1-7127-4eaf-b6fc-45c28a5277b0"),
+                        Status = ParticipationStatus.Accepted
+                    }
+                }
+            };
+
+            var bundle = new Bundle
+            {
+                IdElement = new Id("18c8c73a-9d96-498f-95c3-3e8c59636177"),
+                Type = Bundle.BundleType.Collection,
+                Entry = new List<Bundle.EntryComponent>
+                {
+                    new Bundle.EntryComponent
+                    {
+                        FullUrl = "Appointment/f1b652f7-61c5-4036-a897-9935b245b30c",
+                        Resource = appointment
+                    }
+                }
+            };
+
+            var settings = new ValidationSettings()
+            {
+                ResourceResolver = ZipSource.CreateValidationSource(),
+            };
+            var validator = new Validator(settings);
+
+            var report = validator.Validate(bundle);
             Assert.IsTrue(report.Success);
         }
     }
