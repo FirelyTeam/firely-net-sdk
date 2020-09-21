7fe49d2b9bfd13795803a4f09f6ac9b1747c7834
Ewout Kramer <e.kramer@furore.com>
2020-09-21 14:59:03 +0200

    rewrite: improvements suggested by PR review

--- build/ChangeTargetFramework.ps1
+++ build/ChangeTargetFramework.ps1
@@ -1,31 +1,26 @@
-﻿Param(
-  [string]$TFparam
+﻿# This script takes a single parameter, the (possible) concatenated list of platforms to build against.
+# There is a sensible default, so it will run without a param as well.
+
+Param(
+  [string]$newTF = "netstandard2.0;net45;netstandard1.6"
 )
 
-$newTF = "netstandard2.0;net45;netstandard1.6"
-
-If (![string]::IsNullOrEmpty($TFparam)) 
-{
-	$newTF = $TFparam
-}
-
-$projectFiles = Get-Childitem .. -Recurse -Include *.csproj
+# Scan for all project files in the /src directory, but exclude test projects
+$projectFiles =  Get-Childitem .. -Recurse -Include *.csproj -Exclude *.Tests.csproj
 
 foreach($currentProjFile in $projectFiles)
 {
-  $isTest = $currentProjFile.Name.Contains(".Tests.csproj")  
-
-  # test files are untouched - they are just using only one platform anyway
-  if($isTest) { continue }
-
   Write "Updating target frameworks for $currentProjFile to: $newTF"
 
+  # read the XML for the project file
   $xml = [xml](get-content $currentProjFile) 
 
+  # If the project specifies a target framework, overwrite it with the new one
   if($xml.Project.PropertyGroup[0].TargetFrameworks)
   {
     $xml.Project.PropertyGroup[0].TargetFrameworks = $newTF
   }
-  
+
+  # save the xml back to the original project file  
   $xml.Save($currentProjFile)  
 }
