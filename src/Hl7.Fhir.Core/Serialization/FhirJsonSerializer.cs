cf6ed7a8267f0dfd6aad7646ed393d42708c1ca1
Marco Visser <m.visser@furore.com>
2020-12-03 20:14:25 +0100

    Revert "Added make use of System.Text.Json library for serializing to string"
    This reverts commit fc79d1683d215ecf7f1b498cadc9ba0d1968c0ad.

--- src/Hl7.Fhir.Core/Serialization/FhirJsonSerializer.cs
+++ src/Hl7.Fhir.Core/Serialization/FhirJsonSerializer.cs
@@ -6,6 +6,7 @@
  * available at https://raw.githubusercontent.com/FirelyTeam/firely-net-sdk/master/LICENSE
  */
 
+using System;
 using Hl7.Fhir.Model;
 using Hl7.Fhir.Rest;
 using Newtonsoft.Json;
@@ -22,19 +23,13 @@ namespace Hl7.Fhir.Serialization
         private FhirJsonSerializationSettings buildFhirJsonWriterSettings() =>
             new FhirJsonSerializationSettings { Pretty = Settings.Pretty, AppendNewLine = Settings.AppendNewLine };
 
+        public string SerializeToString(Base instance, SummaryType summary = SummaryType.False, string[] elements = null) => 
+            MakeElementStack(instance, summary, elements).ToJson(buildFhirJsonWriterSettings());
 
-
-#if NETSTANDARD2_0
-        public string SerializeToString(Base instance, SummaryType summary = SummaryType.False, string[] elements = null) =>
-            MakeElementStack(instance, summary, elements).ToJsonText(buildFhirJsonWriterSettings());
-#else
-        public string SerializeToString(Base instance, SummaryType summary = SummaryType.False, string[] elements = null) =>
-           MakeElementStack(instance, summary, elements).ToJson(buildFhirJsonWriterSettings());
-#endif
-        public byte[] SerializeToBytes(Base instance, SummaryType summary = SummaryType.False, string[] elements = null) =>
+        public byte[] SerializeToBytes(Base instance, SummaryType summary = SummaryType.False, string[] elements = null) => 
             MakeElementStack(instance, summary, elements).ToJsonBytes(buildFhirJsonWriterSettings());
 
-        public JObject SerializeToDocument(Base instance, SummaryType summary = SummaryType.False, string[] elements = null) =>
+        public JObject SerializeToDocument(Base instance, SummaryType summary = SummaryType.False, string[] elements = null) => 
             MakeElementStack(instance, summary, elements).ToJObject(buildFhirJsonWriterSettings());
 
         public void Serialize(Base instance, JsonWriter writer, SummaryType summary = SummaryType.False, string[] elements = null) =>
