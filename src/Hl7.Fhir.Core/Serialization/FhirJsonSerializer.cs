d7f7afd4054f7ae5f4096c0758e4f4d7acf73a19
Marco Visser <m.visser@furore.com>
2020-12-03 20:14:17 +0100

    Revert "Fixed compiler error"
    This reverts commit 3bd3b3f1a033933daf7613d76b00926330681e62.

--- src/Hl7.Fhir.Core/Serialization/FhirJsonSerializer.cs
+++ src/Hl7.Fhir.Core/Serialization/FhirJsonSerializer.cs
@@ -26,12 +26,11 @@ namespace Hl7.Fhir.Serialization
 
 #if NETSTANDARD2_0
         public string SerializeToString(Base instance, SummaryType summary = SummaryType.False, string[] elements = null) =>
-            MakeElementStack(instance, summary, elements).ToJson(buildFhirJsonWriterSettings());
+            MakeElementStack(instance, summary, elements).ToJsonText(buildFhirJsonWriterSettings());
 #else
         public string SerializeToString(Base instance, SummaryType summary = SummaryType.False, string[] elements = null) =>
            MakeElementStack(instance, summary, elements).ToJson(buildFhirJsonWriterSettings());
 #endif
-
         public byte[] SerializeToBytes(Base instance, SummaryType summary = SummaryType.False, string[] elements = null) =>
             MakeElementStack(instance, summary, elements).ToJsonBytes(buildFhirJsonWriterSettings());
 
