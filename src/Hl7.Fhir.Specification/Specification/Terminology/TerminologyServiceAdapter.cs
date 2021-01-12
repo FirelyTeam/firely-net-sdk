d0876b106623d3d7c3667c17e937c5ce6a7bfbd7
Marco Visser <m.visser@furore.com>
2021-01-12 18:30:51 +0100

    Use new method ValueSetValidateCode

--- src/Hl7.Fhir.Specification/Specification/Terminology/TerminologyServiceAdapter.cs
+++ src/Hl7.Fhir.Specification/Specification/Terminology/TerminologyServiceAdapter.cs
@@ -1,8 +1,9 @@
-﻿using Hl7.Fhir.ElementModel.Types;
+﻿using Hl7.Fhir.Model;
 using Hl7.Fhir.Specification.Terminology;
-using Hl7.Fhir.Validation;
+using Hl7.Fhir.Utility;
 using Hl7.Fhir.Validation.Schema;
 using System.Linq;
+using P = Hl7.Fhir.ElementModel.Types;
 using T = System.Threading.Tasks;
 
 namespace Hl7.Fhir.Specification.Specification.Terminology
@@ -16,24 +17,47 @@ namespace Hl7.Fhir.Specification.Specification.Terminology
             _service = service;
         }
 
-        private Model.Coding ToFhirPrimitive(Code coding) =>
+        private Model.Coding ToFhirPrimitive(P.Code coding) =>
             coding is null ? null : new Model.Coding() { Code = coding?.Value, System = coding?.System, Display = coding?.Display };
 
-        private Model.CodeableConcept ToFhirPrimitive(Concept concept) =>
+        private Model.CodeableConcept ToFhirPrimitive(P.Concept concept) =>
             concept is null ? null :
             new Model.CodeableConcept() { Coding = concept?.Codes.Select(cd => ToFhirPrimitive(cd)).ToList(), Text = concept?.Display };
 
-
-        public T.Task<Assertions> ValidateCode(string canonical = null, string context = null, string code = null, string system = null, string version = null, string display = null, Code coding = null, Concept codeableConcept = null, DateTime date = null, bool? @abstract = null, string displayLanguage = null)
+        private static IssueSeverity? ConvertToSeverity(OperationOutcome.IssueSeverity? severity) => severity switch
         {
-            var outcome = _service.ValidateCode(canonical: canonical, context: context, code: code, system: system, version: version, display: display,
+            OperationOutcome.IssueSeverity.Fatal => IssueSeverity.Fatal,
+            OperationOutcome.IssueSeverity.Error => IssueSeverity.Error,
+            OperationOutcome.IssueSeverity.Warning => IssueSeverity.Warning,
+            _ => IssueSeverity.Information,
+        };
 
-                    coding: ToFhirPrimitive(coding), codeableConcept: ToFhirPrimitive(codeableConcept), date: null,  // todo convert from system types to Fhir Types
+        public async T.Task<Assertions> ValidateCode(string canonical = null, string context = null, string code = null, string system = null, string version = null, string display = null, P.Code coding = null, P.Concept codeableConcept = null, P.DateTime date = null, bool? @abstract = null, string displayLanguage = null)
+        {
+            var parameters = new ValidateCodeParameters()
+               .WithValueSet(url: canonical, context: context)
+               .WithCode(code: code, system: system, systemVersion: version, display: display, displayLanguage: displayLanguage)
+               .WithCoding(ToFhirPrimitive(coding))
+               .WithCodeableConcept(ToFhirPrimitive(codeableConcept))
+               .WithAbstract(@abstract)
+               .Build();
 
-                    @abstract: @abstract, displayLanguage: displayLanguage);
+            var resultParms = await _service.ValueSetValidateCode(parameters);
 
 
-            return T.Task.FromResult(outcome.ToAssertions());
+            var result = resultParms.GetSingleValue<FhirBoolean>("result")?.Value ?? false;
+            var message = resultParms.GetSingleValue<FhirString>("message")?.Value;
+
+            var assertions = Assertions.Empty;
+
+            if (message is { })
+            {
+                var severity = result ? OperationOutcome.IssueSeverity.Warning : OperationOutcome.IssueSeverity.Error;
+                var issue = new IssueAssertion(-1, null, message, ConvertToSeverity(severity));
+                assertions += issue;
+            }
+
+            return assertions;
         }
     }
 }
