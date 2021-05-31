38b1bcdaac979b89b0713780fc1c1dafeebdcf94
Marten Smits <m.smits@furore.com>
2021-05-31 15:33:37 +0200

    added extra testcase

--- src/Hl7.Fhir.Core.Tests/Model/ModelTests.cs
+++ src/Hl7.Fhir.Core.Tests/Model/ModelTests.cs
@@ -86,11 +86,14 @@ namespace Hl7.Fhir.Tests.Model
             fdt = new FhirDateTime("2021-03-18T12:22:35.999Z");
             Assert.IsTrue(fdt.TryToDateTimeOffset(out var dto3));
             Assert.AreEqual("2021-03-18T12:22:35.9990000+00:00", dto3.ToString("o"));
+            
+            fdt = new FhirDateTime("2021-03-18T12:22:35.1234+04:00");
+            Assert.IsTrue(fdt.TryToDateTimeOffset(out var dto4));
+            Assert.AreEqual("2021-03-18T12:22:35.1234000+04:00", dto4.ToString("o"));
 
             fdt = new FhirDateTime("2021-03-18T12:22:35.999");
             Assert.AreEqual("2021-03-18T12:22:35.999", fdt.Value);
-            Assert.IsFalse(fdt.TryToDateTimeOffset(out var dto4));
-          
+            Assert.IsFalse(fdt.TryToDateTimeOffset(out var dto5));
         }
 
         [TestMethod]
