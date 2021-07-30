ad827ef4878f264e40ba391a5fa848c0e79e6544
Ewout Kramer <e.kramer@furore.com>
2021-07-30 16:50:45 +0200

    Added benchmark project, and included benchmark test for ModelInspector

---
+++ src/Benchmarks/Program.cs
@@ -0,0 +1,12 @@
+﻿using BenchmarkDotNet.Running;
+
+namespace Benchmarks
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            _ = BenchmarkRunner.Run<ModelInspectorBenchmarks>();
+        }
+    }
+}
