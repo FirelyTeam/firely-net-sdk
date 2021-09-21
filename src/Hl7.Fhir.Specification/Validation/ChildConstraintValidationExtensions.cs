d6455d1b56e465e9364501d202a7ac7033d68807
Marco Visser <marco@fire.ly>
2021-09-21 16:21:56 +0200

    Validate only the targetprofiles on ChildDefs to prevent Stack overflow
    # Conflicts:
    #	src/Hl7.Fhir.Specification/Validation/TypeRefValidationExtensions.cs

--- src/Hl7.Fhir.Specification/Validation/ChildConstraintValidationExtensions.cs
+++ src/Hl7.Fhir.Specification/Validation/ChildConstraintValidationExtensions.cs
@@ -27,7 +27,7 @@ namespace Hl7.Fhir.Validation
             validator.Trace(outcome, "Start validation of inlined child constraints for '{0}'".FormatWith(definition.Path), Issue.PROCESSING_PROGRESS, instance);
 
             // validate the type on the parent of children. If this is a reference type, it will follow that reference as well
-            outcome.Add(validator.ValidateType(definition.Current, instance));
+            outcome.Add(validator.ValidateTargetProfiles(definition.Current, instance));
 
             var matchResult = ChildNameMatcher.Match(definition, instance);
 
