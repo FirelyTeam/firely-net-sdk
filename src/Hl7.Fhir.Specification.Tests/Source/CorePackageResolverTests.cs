a7036146227188cc7ab6a31a545fa74c2ea94039
Marten Smits <m.smits@furore.com>
2021-10-01 12:58:39 +0200

    added core package resolver

---
+++ src/Hl7.Fhir.Specification.Tests/Source/CorePackageResolverTests.cs
@@ -0,0 +1,20 @@
+﻿using FluentAssertions;
+using Hl7.Fhir.Specification.Source;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Threading.Tasks;
+
+namespace Firely.Fhir.Packages.Tests
+{
+    [TestClass]
+    public class CorePackageResolverTests
+    {
+        [TestMethod]
+        public async Task ResolvesStructureDefinitions()
+        {
+            var resolver = new CorePackageSource();
+
+            var pat = await resolver.ResolveByCanonicalUriAsync("http://hl7.org/fhir/StructureDefinition/Patient");
+            pat.Should().NotBeNull();
+        }
+    }
+}
---
+++ src/Hl7.Fhir.Specification/Specification/Source/CorePackageSource.cs
@@ -0,0 +1,27 @@
+﻿using Hl7.Fhir.Model;
+using System.IO;
+using System.Threading.Tasks;
+
+namespace Hl7.Fhir.Specification.Source
+{
+    public class CorePackageSource : IAsyncResourceResolver
+    {
+        private const string PACKAGENAME = "hl7.fhir.r3.core-3.0.2.tgz";
+        private NpmPackageResolver _resolver;
+
+        public CorePackageSource()
+        {
+            var inspector = ModelInfo.ModelInspector;
+            var corePackagePath = Path.Combine(Directory.GetCurrentDirectory(), PACKAGENAME);
+            _resolver = new NpmPackageResolver(corePackagePath, inspector);
+        }
+        public async Task<Resource> ResolveByCanonicalUriAsync(string uri)
+        {
+            return await _resolver.ResolveByCanonicalUriAsync(uri);
+        }
+        public async Task<Resource> ResolveByUriAsync(string uri)
+        {
+            return await _resolver.ResolveByCanonicalUriAsync(uri);
+        }
+    }
+}
