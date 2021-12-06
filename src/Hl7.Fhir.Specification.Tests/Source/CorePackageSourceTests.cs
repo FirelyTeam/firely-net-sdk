ab34217bbe0f0e1c075c87de496f1d0e23ea333b
Marten Smits <m.smits@furore.com>
2021-12-06 16:33:14 +0100

    Added features to support IConformanceSource to package source

--- src/Hl7.Fhir.Specification.Tests/Source/CorePackageSourceTests.cs
+++ src/Hl7.Fhir.Specification.Tests/Source/CorePackageSourceTests.cs
@@ -47,5 +47,45 @@ namespace Firely.Fhir.Packages.Tests
 
             artifact.Should().StartWith("{\"resourceType\":\"StructureDefinition\",\"id\":\"Patient\"");
         }
+
+        [TestMethod]
+        public void TestListResourceUris()
+        {
+            var resolver = new CorePackageSource();
+
+            //check StructureDefinitions
+            var names = resolver.ListResourceUris();
+            names.Should().Contain("http://hl7.org/fhir/StructureDefinition/Patient");
+            names.Should().Contain("http://hl7.org/fhir/administrative-gender");
+        }
+
+        [TestMethod]
+        public void TestGetCodeSystemByValueSet()
+        {
+            var resolver = new CorePackageSource();
+            var cs = resolver.FindCodeSystemByValueSet("http://hl7.org/fhir/ValueSet/address-type");
+            cs.Should().NotBeNull();
+            cs.Url.Should().Be("http://hl7.org/fhir/address-type");
+        }
+
+        [TestMethod]
+        public void TestGetConceptMap()
+        {
+            var resolver = new CorePackageSource();
+            var cms = resolver.FindConceptMaps(sourceUri: "http://hl7.org/fhir/ValueSet/data-absent-reason", targetUri: "http://hl7.org/fhir/ValueSet/v3-NullFlavor");
+            cms.Should().NotBeEmpty();
+            cms.Should().Contain(c => c.Url == "http://hl7.org/fhir/ConceptMap/cm-data-absent-reason-v3");
+            cms.Should().NotContain(c => c.Url == "http://hl7.org/fhir/ConceptMap/cm-contact-point-use-v3");
+        }
+
+        [TestMethod]
+        public void TestGetNamingSystem()
+        {
+            var resolver = new CorePackageSource();
+            var ns = resolver.FindNamingSystem("http://snomed.info/sct");
+            ns.Should().NotBeNull();
+            ns.UniqueId.Should().Contain(i => i.Value == "http://snomed.info/sct");
+            ns.UniqueId.Should().Contain(i => i.Value == "2.16.840.1.113883.6.96");
+        }
     }
 }
--- src/Hl7.Fhir.Specification/Specification/Source/CorePackageSource.cs
+++ src/Hl7.Fhir.Specification/Specification/Source/CorePackageSource.cs
@@ -1,17 +1,22 @@
 ﻿using Hl7.Fhir.Model;
+using Hl7.Fhir.Utility;
 using System.Collections.Generic;
 using System.IO;
+using System.Linq;
 using System.Threading.Tasks;
 
 #nullable enable
 namespace Hl7.Fhir.Specification.Source
 {
-    public class CorePackageSource : IAsyncResourceResolver, IArtifactSource
+    /// <summary>Reads FHIR artifacts (Profiles, ValueSets, ...) from the FHIR Core package</summary>
+    public class CorePackageSource : IAsyncResourceResolver, IArtifactSource, IConformanceSource
     {
         private const string PACKAGENAME = "hl7.fhir.r3.core-3.0.2.tgz";
         private const string PACKAGENAME_EXPANSIONS = "hl7.fhir.r3.expansions-3.0.2.tgz";
         private NpmPackageResolver _resolver;
 
+        /// <summary>Create a new <see cref="CorePackageSource()"/> instance to read FHIR artifacts from the FHIR core package.</summary>
+        /// <returns>A new <see cref="CorePackageSource()"/> instance.</returns>
         public CorePackageSource()
         {
             var inspector = ModelInfo.ModelInspector;
@@ -20,23 +25,86 @@ namespace Hl7.Fhir.Specification.Source
             _resolver = new NpmPackageResolver(inspector, corePackagePath, coreExpansionPackagePath);
         }
 
+        ///<inheritdoc/>
         public async Task<Resource?> ResolveByCanonicalUriAsync(string uri)
         {
-            return await _resolver.ResolveByCanonicalUriAsync(uri);
+            return await _resolver.ResolveByCanonicalUriAsync(uri).ConfigureAwait(false);
         }
+
+        ///<inheritdoc/>
         public async Task<Resource?> ResolveByUriAsync(string uri)
         {
-            return await _resolver.ResolveByCanonicalUriAsync(uri);
+            return await _resolver.ResolveByCanonicalUriAsync(uri).ConfigureAwait(false);
         }
+
+        ///<inheritdoc/>
         public IEnumerable<string> ListArtifactNames()
         {
             return _resolver.ListArtifactNames();
         }
+
+        ///<inheritdoc/>
         public Stream? LoadArtifactByName(string artifactName)
         {
             return _resolver.LoadArtifactByName(artifactName);
         }
 
+        ///<inheritdoc/>
+        public IEnumerable<string> ListResourceUris(ResourceType? filter = null)
+        {
+            return _resolver.ListResourceUris(filter?.GetLiteral());
+        }
+
+        ///<inheritdoc/>
+        public async Task<CodeSystem?> FindCodeSystemByValueSetAsync(string valueSetUri)
+        {
+            var resource = await _resolver.FindCodeSystemByValueSet(valueSetUri).ConfigureAwait(false);
+            return resource as CodeSystem;
+        }
+
+        ///<inheritdoc/>
+        public CodeSystem? FindCodeSystemByValueSet(string valueSetUri)
+        {
+            return TaskHelper.Await(() => FindCodeSystemByValueSetAsync(valueSetUri));
+        }
+
+        ///<inheritdoc/>
+        public async Task<IEnumerable<ConceptMap>?> FindConceptMapsAsync(string? sourceUri = null, string? targetUri = null)
+        {
+            var resources = await _resolver.FindConceptMaps(sourceUri, targetUri).ConfigureAwait(false);
+            return resources == null ? null : resources.Cast<ConceptMap>();
+        }
+
+        ///<inheritdoc/>
+        public IEnumerable<ConceptMap>? FindConceptMaps(string? sourceUri = null, string? targetUri = null)
+        {
+            return TaskHelper.Await(() => FindConceptMapsAsync(sourceUri, targetUri));
+        }
+
+        ///<inheritdoc/>
+        public async Task<NamingSystem?> FindNamingSystemAsync(string uniqueId)
+        {
+            var resource = await _resolver.FindNamingSystemByUniqueId(uniqueId);
+            return resource as NamingSystem;
+        }
+
+        ///<inheritdoc/>
+        public NamingSystem? FindNamingSystem(string valueSetUri)
+        {
+            return TaskHelper.Await(() => FindNamingSystemAsync(valueSetUri));
+        }
+
+        ///<inheritdoc/>
+        public Resource? ResolveByUri(string uri)
+        {
+            return TaskHelper.Await(() => ResolveByUriAsync(uri));
+        }
+
+        ///<inheritdoc/>
+        public Resource? ResolveByCanonicalUri(string uri)
+        {
+            return TaskHelper.Await(() => ResolveByCanonicalUriAsync(uri));
+        }
     }
 }
 
