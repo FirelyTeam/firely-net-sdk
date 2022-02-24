06a4650d24f610baf4e01aa745a2b698a64bfb44
Marten Smits <m.smits@furore.com>
2022-02-24 12:16:49 +0100

    added some documentation and nullability

--- src/Hl7.Fhir.Core/FhirPath/FhirPathExtensions.cs
+++ src/Hl7.Fhir.Core/FhirPath/FhirPathExtensions.cs
@@ -6,6 +6,8 @@
  * available at https://raw.githubusercontent.com/FirelyTeam/firely-net-sdk/master/LICENSE
  */
 
+#nullable enable
+
 using Hl7.Fhir.ElementModel;
 using Hl7.Fhir.Model;
 using Hl7.Fhir.Utility;
@@ -18,46 +20,88 @@ namespace Hl7.Fhir.FhirPath
 {
     public static class FhirPathExtensions
     {
-        public static Func<string, ITypedElement> ToFhirPathResolver(this Func<string, Resource> resolver)
+        /// <summary>
+        /// Converts results of a resolver from Resource to ITypedElement
+        /// </summary>
+        /// <param name="resolver">results of a resolver as Resource</param>
+        /// <returns>Result of the convertion to ITypedElement</returns>
+        public static Func<string, ITypedElement?> ToFhirPathResolver(this Func<string, Resource> resolver)
         {
             return navResolver;
 
-            ITypedElement navResolver(string url)
+            ITypedElement? navResolver(string url)
             {
                 var resource = resolver(url);
                 return resource?.ToTypedElement();
             }
         }
-
-        public static IEnumerable<Base> Select(this Base input, string expression, FhirEvaluationContext ctx = null)
+        /// <summary>
+        /// Evaluates an expression against a given context and returns the result(s)
+        /// </summary>
+        /// <param name="input">Input on which the expression is being evaluated</param>
+        /// <param name="expression">Expression which is to be evaluated</param>
+        /// <param name="ctx">Context of the evaluation</param>
+        /// <returns>The result(s) of the expression</returns>
+        public static IEnumerable<Base> Select(this Base input, string expression, FhirEvaluationContext? ctx = null)
         {
             var inputNav = input.ToTypedElement();
             var result = inputNav.Select(expression, ctx ?? FhirEvaluationContext.CreateDefault());
             return result.ToFhirValues();
         }
 
-        public static object Scalar(this Base input, string expression, FhirEvaluationContext ctx = null)
+        /// <summary>
+        /// Evaluates an expression against a given context and returns a single result
+        /// </summary>
+        /// <param name="input">Input on which the expression is being evaluated</param>
+        /// <param name="expression">Expression which is to be evaluated</param>
+        /// <param name="ctx">Context of the evaluation</param>
+        /// <returns>The single result of the expression, and null if the expression returns multiple results</returns>
+        public static object? Scalar(this Base input, string expression, FhirEvaluationContext? ctx = null)
         {
             var inputNav = input.ToTypedElement();
             return inputNav.Scalar(expression, ctx ?? FhirEvaluationContext.CreateDefault());
         }
 
-        public static bool Predicate(this Base input, string expression, FhirEvaluationContext ctx = null)
+        /// <summary>
+        /// Evaluates an expression and returns true for expression being evaluated as true or empty, otherwise false.
+        /// </summary>
+        /// <param name="input">Input on which the expression is being evaluated</param>
+        /// <param name="expression">Expression which is to be evaluated</param>
+        /// <param name="ctx">Context of the evaluation</param>
+        /// <returns>True if expression returns true of empty, otheriwse false</returns>
+        public static bool Predicate(this Base input, string expression, FhirEvaluationContext? ctx = null)
         {
             var inputNav = input.ToTypedElement();
             return inputNav.Predicate(expression, ctx ?? FhirEvaluationContext.CreateDefault());
         }
 
-        public static bool IsTrue(this Base input, string expression, FhirEvaluationContext ctx = null)
+        /// <summary>
+        /// Evaluates an expression and returns true for expression being evaluated as true, and false if the expression returns false or empty.
+        /// </summary>
+        /// <param name="input">Input on which the expression is being evaluated</param>
+        /// <param name="expression">Expression which is to be evaluated</param>
+        /// <param name="ctx">Context of the evaluation</param>
+        /// <returns>True if expression returns true , and false if expression returns empty of false.</returns>
+        public static bool IsTrue(this Base input, string expression, FhirEvaluationContext? ctx = null)
         {
             var inputNav = input.ToTypedElement();
-            return inputNav.Predicate(expression, ctx ?? FhirEvaluationContext.CreateDefault());
+            return inputNav.IsTrue(expression, ctx ?? FhirEvaluationContext.CreateDefault());
         }
 
-        public static bool IsBoolean(this Base input, string expression, bool value, FhirEvaluationContext ctx = null)
+        /// <summary>
+        ///Evaluates if the result of an expression is equal to a given boolean.
+        /// </summary>
+        /// <param name="input">Input on which the expression is being evaluated</param>
+        /// <param name="value">Boolean that is to be compared to the result of the expression</param>
+        /// <param name="expression">Expression which is to be evaluated</param>
+        /// <param name="ctx">Context of the evaluation</param>
+        /// <returns>True if the result of an expression is equal to a given boolean, otherwise false</returns>
+        public static bool IsBoolean(this Base input, string expression, bool value, FhirEvaluationContext? ctx = null)
         {
             var inputNav = input.ToTypedElement();
             return inputNav.IsBoolean(expression, value, ctx ?? FhirEvaluationContext.CreateDefault());
         }
     }
 }
+
+#nullable restore
