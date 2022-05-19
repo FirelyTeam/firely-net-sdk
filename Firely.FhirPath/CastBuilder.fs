a8b821cead81bf64b661d1496b880d4a951069eb
Ewout Kramer <e.kramer@furore.com>
2022-05-19 16:30:44 +0200

    Initial few lines

---
+++ Firely.FhirPath/CastBuilder.fs
@@ -0,0 +1,30 @@
+﻿namespace Firely.FhirPath
+
+open System
+open System.Collections.Generic
+open System.Linq.Expressions
+
+type GenericParameterAssignments() =
+    inherit Dictionary<Type,Type>()
+
+type GeneratedExpression = 
+    {
+        Generated: Expression;
+        Complexity: int;
+        Success: bool;
+        AssignmentHypothesis: option<Type*Type>;
+    }
+
+type ICastBuilder = 
+    abstract member CanApproach: source:Type -> target:Type -> bool
+    abstract member Convert: source:Expression -> target:Type -> gp:GenericParameterAssignments -> disp:ConverterCollection -> GeneratedExpression
+
+and ConverterCollection([<ParamArray>] converters: ICastBuilder[]) = 
+    member val Converters = converters with get, set
+
+    member cc.Convert(source: Expression, target: Type, gp: GenericParameterAssignments) : GeneratedExpression = 
+        let converter = cc.Converters |> Array.tryFind (fun c -> c.CanApproach source.Type target)
+
+        match converter with
+        | Some c -> c.Convert source target gp cc
+        | None -> { Generated = source; Complexity = 0; Success = false; AssignmentHypothesis = None }
---
+++ Firely.FhirPath/TypeExtensions.fs
@@ -0,0 +1,25 @@
+﻿module internal Firely.FhirPath.TypeExtensions
+
+open System
+open System.Collections.Generic
+
+type Type with
+  member t.GetContainerInterface(containerType: Type): Type option =
+    seq { yield! t.GetInterfaces(); yield containerType }
+    |> Seq.tryFind (fun x -> x.IsGenericType && x.GetGenericTypeDefinition() = containerType)
+    
+  member t.IsContainerOf(containerType: Type) = t.GetContainerInterface(containerType).IsSome
+  
+  member t.IsCollection = t.IsContainerOf(typedefof<ICollection<_>>)
+
+  member t.GetContainerParamOf(containerType: Type): Type option = 
+    let getExactlyOneArgument (t: Type) = t.GetGenericArguments() |> Array.tryExactlyOne
+    t.GetContainerInterface(containerType) |> Option.bind getExactlyOneArgument
+
+  member t.GetCollectionElement(): Type option = t.GetContainerInterface(typedefof<ICollection<_>>)
+
+  member t.GetDelegateParameters(): Type[] = 
+    if t.IsGenericType && t.IsAssignableTo(typeof<Delegate>) then 
+      t.GetGenericArguments()
+    else 
+      Array.empty
