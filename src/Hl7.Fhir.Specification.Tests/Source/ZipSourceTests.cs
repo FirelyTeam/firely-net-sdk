43d582c924ca85aa98a547014b0facb11ee0884b
Marten Smits <m.smits@furore.com>
2022-08-04 15:32:47 +0200

    fix: corrected fullUrl for valueset-compose-include-valueSetTitle

--- src/Hl7.Fhir.Specification.Tests/Source/ZipSourceTests.cs
+++ src/Hl7.Fhir.Specification.Tests/Source/ZipSourceTests.cs
@@ -1,4 +1,7 @@
-﻿using Hl7.Fhir.Specification.Source;
+﻿using FluentAssertions;
+using Hl7.Fhir.Model;
+using Hl7.Fhir.Serialization;
+using Hl7.Fhir.Specification.Source;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using System.IO;
 using System.Linq;
@@ -33,5 +36,23 @@ namespace Hl7.Fhir.Specification.Tests.Source
             Assert.IsNotNull(summaries, "Collection of summeries should not be null");
             Assert.AreEqual(1, summaries.Count(), "In the zipfile there is 1 resource in the root folder.");
         }
+
+
+        //issue #36678
+        [TestMethod]
+        public void TestIncorrectFullUrlForValuesetComposeIncludeValueSetTitle()
+        {
+            var _resolver = ZipSource.CreateValidationSource();
+            var stream = _resolver.LoadArtifactByName("extension-definitions.xml");
+            var text = new StreamReader(stream).ReadToEnd();
+            var bundle = new FhirXmlParser().Parse<Bundle>(text);
+
+            var extensionEntry = bundle.Entry.Where(e => e.FullUrl == "http://hl7.org/fhir/StructureDefinition/valueset-compose-include-valueSetTitle").FirstOrDefault();
+            extensionEntry.Should().NotBeNull();
+            var sd = extensionEntry.Resource as StructureDefinition;
+            sd.Url.Should().Be("http://hl7.org/fhir/StructureDefinition/valueset-compose-include-valueSetTitle");
+
+        }
+
     }
 }
