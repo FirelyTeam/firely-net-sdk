82fa665b8db6510721de390bce76dc40caa38b28
Marten Smits <m.smits@furore.com>
2022-09-14 09:46:27 +0200

    move null check on instanceType to still allow validation on custom resources

--- src/Hl7.Fhir.Specification/Validation/TypeRefValidationExtensions.cs
+++ src/Hl7.Fhir.Specification/Validation/TypeRefValidationExtensions.cs
@@ -38,15 +38,16 @@ namespace Hl7.Fhir.Validation
                 //find out what type is present in the instance data
                 // (e.g. deceased[Boolean], or _resourceType in json). This is exposed by IElementNavigator.TypeName.
                 var instanceType = ModelInfo.FhirTypeNameToFhirType(instance.InstanceType);
-                if (instanceType is not null)
+
+                if (choices.Count() > 1)
                 {
-                    if (choices.Count() > 1)
+                    if (instanceType is not null)
                     {
                         // The next statements are just an optimalization, without them, we would do an ANY validation
                         // against *all* choices, what we do here is pre-filtering for sensible choices, and report if there isn't
                         // any.
                         var applicableChoices = typeRefs.Where(tr => !tr.Code.StartsWith("http:"))
-                                        .Where(tr => ModelInfo.IsInstanceTypeFor(ModelInfo.FhirTypeNameToFhirType(tr.Code).Value, instanceType.Value));
+                                    .Where(tr => ModelInfo.IsInstanceTypeFor(ModelInfo.FhirTypeNameToFhirType(tr.Code).Value, instanceType.Value));
 
                         // Instance typename must be one of the applicable types in the choice
                         if (applicableChoices.Any())
@@ -57,27 +58,32 @@ namespace Hl7.Fhir.Validation
                         {
                             var choiceList = String.Join(",", choices.Select(t => "'" + t + "'"));
                             validator.Trace(outcome, $"Type specified in the instance ('{instance.InstanceType}') is not one of the allowed choices ({choiceList})",
-                                     Issue.CONTENT_ELEMENT_HAS_INCORRECT_TYPE, instance);
+                                        Issue.CONTENT_ELEMENT_HAS_INCORRECT_TYPE, instance);
                         }
                     }
-                    else if (choices.Count() == 1)
+                    else
+                    {
+                        validator.Trace(outcome, $"Instance indicates the element is of type '{instance.InstanceType}', which is not a known FHIR core type.",
+                                     Issue.CONTENT_ELEMENT_CHOICE_INVALID_INSTANCE_TYPE, instance);
+                    }
+                }
+                else if (choices.Count() == 1)
+                {
+                    if (instanceType is not null)
                     {
                         // Check if instance is of correct type
                         var isCorrectType = ModelInfo.IsInstanceTypeFor(ModelInfo.FhirTypeNameToFhirType(choices.Single()).Value, instanceType.Value);
                         if (!isCorrectType)
                         {
                             validator.Trace(outcome, $"Type specified in the instance ('{instance.InstanceType}') is not of the expected type ('{choices.Single()}')",
-                                           Issue.CONTENT_ELEMENT_HAS_INCORRECT_TYPE, instance);
+                                            Issue.CONTENT_ELEMENT_HAS_INCORRECT_TYPE, instance);
                         }
-                        // Only one type present in list of typerefs, all of the typerefs are candidates
-                        outcome.Include(validator.ValidateTypeReferences(typeRefs, instance, state, validateProfiles));
                     }
+                    // Only one type present in list of typerefs, all of the typerefs are candidates
+                    outcome.Include(validator.ValidateTypeReferences(typeRefs, instance, state, validateProfiles));
                 }
-                else
-                {
-                    validator.Trace(outcome, $"Instance indicates the element is of type '{instance.InstanceType}', which is not a known FHIR core type.",
-                                 Issue.CONTENT_ELEMENT_CHOICE_INVALID_INSTANCE_TYPE, instance);
-                }
+
+
             }
             else
             {
