e69716f00f3ee562c4d6086180a5cea5ecbedf38
Marten Smits <m.smits@furore.com>
2023-02-03 16:33:36 +0100

    Update src/Hl7.Fhir.Base/Rest/TransactionBuilder.cs
    Co-authored-by: Marco Visser <marco@fire.ly>

--- src/Hl7.Fhir.Base/Rest/TransactionBuilder.cs
+++ src/Hl7.Fhir.Base/Rest/TransactionBuilder.cs
@@ -30,7 +30,7 @@ namespace Hl7.Fhir.Rest
         /// Create a builder to describe a FHIR transaction Bundle
         /// </summary>
         /// <param name="baseUrl">URL of the FHIR server that is going to execute the transaction/batch</param>
-        /// <param name="type">Specify is the server is suppose to perform a "batch" or a "transaction"</param>
+        /// <param name="type">Specify that the server should perform a "batch" or a "transaction"</param>
         public TransactionBuilder(string baseUrl, Bundle.BundleType type = Bundle.BundleType.Batch)
         {
             _result = new Bundle()
