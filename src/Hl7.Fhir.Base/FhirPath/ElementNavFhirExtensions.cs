05922cbab995685d88eb6359fcdbabc3023619e1
Marco Visser <marco@fire.ly>
2023-04-19 12:28:01 +0200

    First working approach

--- src/Hl7.Fhir.Base/FhirPath/ElementNavFhirExtensions.cs
+++ src/Hl7.Fhir.Base/FhirPath/ElementNavFhirExtensions.cs
@@ -2,9 +2,12 @@ using Hl7.Fhir.ElementModel;
 using Hl7.Fhir.Model;
 using Hl7.FhirPath;
 using Hl7.FhirPath.Expressions;
+using Hl7.FhirPath.Functions;
 using System;
 using System.Collections.Generic;
+using System.Globalization;
 using System.Linq;
+using System.Text;
 using P = Hl7.Fhir.ElementModel.Types;
 
 namespace Hl7.Fhir.FhirPath
@@ -32,6 +35,16 @@ namespace Hl7.Fhir.FhirPath
             // lets keep both to keep everyone happy.
             t.Add("htmlchecks", (ITypedElement f) => f.HtmlChecks(), doNullProp: false);
             t.Add("htmlChecks", (ITypedElement f) => f.HtmlChecks(), doNullProp: false);
+
+            t.Add("lowBoundary", (decimal d, long precision) => AdjustBoundaryDecimal(d, precision, substract), doNullProp: false);
+            t.Add("lowBoundary", (decimal d) => AdjustBoundaryDecimal(d, null, substract), doNullProp: false);
+            t.Add("lowBoundary", (P.Any a, long precision) => LowBoundary(a, precision), doNullProp: false);
+            t.Add("lowBoundary", (P.Any a) => LowBoundary(a, null), doNullProp: false);
+
+            t.Add("highBoundary", (decimal d, long precision) => AdjustBoundaryDecimal(d, precision, add), doNullProp: false);
+            t.Add("highBoundary", (decimal d) => AdjustBoundaryDecimal(d, null, add), doNullProp: false);
+            t.Add("highBoundary", (P.Any a, long precision) => HighBoundary(a, precision), doNullProp: false);
+            t.Add("highBoundary", (P.Any a) => HighBoundary(a, null), doNullProp: false);
 
             return t;
 
@@ -91,6 +104,162 @@ namespace Hl7.Fhir.FhirPath
                     _ => (Base)result
                 };
             });
+        }
+
+
+        internal static decimal? AdjustBoundaryDecimal(decimal? input, long? precision, Func<decimal, decimal, decimal> op)
+        {
+            if (input is null) return null;
+
+            var decimalParts = input.Value.ToString(CultureInfo.InvariantCulture).Split('.');
+
+            // how many digits after the decimal point?
+            var fractionalDigits = decimalParts.Length > 1 ? decimalParts.Last().Length : 0;
+
+            decimal adjustment = 5 * (decimal)Math.Pow(10, -(fractionalDigits + 1)); // 0.5, 0.05, 0.005, etc
+
+            input = op(input.Value, adjustment);
+
+            if (precision is not null)
+            {
+                StringBuilder precisionAsString = new("0.");
+                precisionAsString.Append('0', (int)precision);
+                input += decimal.Parse(precisionAsString.ToString(), NumberStyles.AllowDecimalPoint, CultureInfo.InvariantCulture);
+            }
+
+            return input;
+        }
+
+        private static decimal substract(decimal operand1, decimal operand2) => operand1 - operand2;
+        private static decimal add(decimal operand1, decimal operand2) => operand1 + operand2;
+
+        internal static P.Any LowBoundary(P.Any input, long? precision)
+        {
+            return input switch
+            {
+                P.Quantity q => AdjustBoundaryQuantity(q, precision, substract),
+                P.Date d => LowBoundaryDateTime(d, precision),
+                P.DateTime dt => LowBoundaryDateTime(dt, precision),
+                P.Time t => LowBoundaryTime(t, precision),
+                _ => null
+            };
+        }
+
+        internal static P.Any HighBoundary(P.Any input, long? precision)
+        {
+            return input switch
+            {
+                P.Quantity q => AdjustBoundaryQuantity(q, precision, add),
+                P.Date d => HighBoundaryDateTime(d, precision),
+                P.DateTime dt => HighBoundaryDateTime(dt, precision),
+                P.Time t => HighBoundaryTime(t, precision),
+                _ => null
+            };
+        }
+
+        internal static P.Quantity AdjustBoundaryQuantity(P.Quantity input, long? precision, Func<decimal, decimal, decimal> op)
+        {
+            var v = AdjustBoundaryDecimal(input.Value, precision, op);
+
+            return new P.Quantity(v.Value, input.Unit, input.System);
+        }
+
+        private static P.DateTimePrecision getNext(P.DateTimePrecision value)
+        {
+            return (from P.DateTimePrecision val in Enum.GetValues(typeof(P.DateTimePrecision))
+                    where val > value
+                    orderby val
+                    select val).DefaultIfEmpty().First();
+        }
+
+        internal static P.DateTime LowBoundaryDateTime(P.DateTime dateTime, long? precision)
+        {
+            P.DateTimePrecision dtPrecision = precision switch
+            {
+                <= 6 => P.DateTimePrecision.Month,
+                <= 8 => P.DateTimePrecision.Day,
+                <= 10 => P.DateTimePrecision.Hour,
+                <= 12 => P.DateTimePrecision.Minute,
+                <= 14 => P.DateTimePrecision.Second,
+                <= 17 => P.DateTimePrecision.Fraction,
+
+                null => getNext(dateTime.Precision),
+                _ => getNext(dateTime.Precision),
+            };
+
+
+            return new(dateTime.ToString(), dateTime.ToDateTimeOffset(TimeSpan.Zero), dtPrecision, dateTime.HasOffset);
+        }
+
+        internal static P.Time LowBoundaryTime(P.Time time, long? precision)
+        {
+            P.DateTimePrecision dtPrecision = precision switch
+            {
+                2 => P.DateTimePrecision.Hour,
+                5 => P.DateTimePrecision.Minute,
+                8 => P.DateTimePrecision.Second,
+                12 => P.DateTimePrecision.Fraction,
+
+                null => getNext(time.Precision),
+                _ => getNext(time.Precision),
+            };
+
+            return P.Time.FromDateTimeOffset(time.ToDateTimeOffset(2023, 04, 18, TimeSpan.Zero), dtPrecision);
+        }
+
+        internal static P.Any HighBoundaryDateTime(P.DateTime dt, long? precision)
+        {
+            TimeSpan offset = dt.HasOffset ? dt.Offset.Value : TimeSpan.Zero;
+
+            DateTimeOffset dto = dt.Precision switch
+            {
+                P.DateTimePrecision.Year => new(dt.Years.Value, 12, 31, 23, 59, 59, 999, offset),
+                P.DateTimePrecision.Month => new(dt.Years.Value, dt.Months.Value, DateTime.DaysInMonth(dt.Years.Value, dt.Months.Value), 23, 59, 59, 999, offset),
+                P.DateTimePrecision.Day => new(dt.Years.Value, dt.Months.Value, dt.Days.Value, 23, 59, 59, 999, offset),
+                P.DateTimePrecision.Hour => new(dt.Years.Value, dt.Months.Value, dt.Days.Value, dt.Hours.Value, 59, 59, 999, offset),
+                P.DateTimePrecision.Minute => new(dt.Years.Value, dt.Months.Value, dt.Days.Value, dt.Hours.Value, dt.Minutes.Value, 59, 999, offset),
+                P.DateTimePrecision.Second => new(dt.Years.Value, dt.Months.Value, dt.Days.Value, dt.Hours.Value, dt.Minutes.Value, dt.Seconds.Value, 999, offset),
+                _ => DateTimeOffset.Now
+            };
+
+            P.DateTimePrecision dtPrecision = precision switch
+            {
+                4 => P.DateTimePrecision.Year,
+                6 => P.DateTimePrecision.Month,
+                8 => P.DateTimePrecision.Day,
+                14 => P.DateTimePrecision.Second,
+                17 or null => P.DateTimePrecision.Fraction,
+                _ => throw new Exception("")
+            };
+
+            return
+                (dtPrecision <= P.DateTimePrecision.Day) ?
+                    P.Date.FromDateTimeOffset(dto, dtPrecision, dt.HasOffset) :
+                    new P.DateTime(dto.ToString(), dto, dtPrecision, dt.HasOffset);
+        }
+
+        internal static P.Time HighBoundaryTime(P.Time time, long? precision)
+        {
+            TimeSpan offset = time.HasOffset ? time.Offset.Value : TimeSpan.Zero;
+
+            DateTimeOffset dto = time.Precision switch
+            {
+                P.DateTimePrecision.Hour => new(2023, 4, 18, time.Hours.Value, 59, 59, 9999, offset),
+                P.DateTimePrecision.Minute => new(2023, 4, 18, time.Hours.Value, time.Minutes.Value, 59, 999, offset),
+                P.DateTimePrecision.Second => new(2023, 4, 18, time.Hours.Value, time.Minutes.Value, time.Seconds.Value, 999, offset),
+                _ => DateTimeOffset.Now
+            };
+
+            P.DateTimePrecision dtPrecision = precision switch
+            {
+                2 => P.DateTimePrecision.Hour,
+                4 => P.DateTimePrecision.Minute,
+                6 => P.DateTimePrecision.Second,
+                9 or null => P.DateTimePrecision.Fraction,
+                _ => throw new Exception("")
+            };
+
+            return P.Time.FromDateTimeOffset(dto, dtPrecision);
         }
     }
 }
--- src/Hl7.Fhir.Support.Tests/FhirPath/FhirPathTests.cs
+++ src/Hl7.Fhir.Support.Tests/FhirPath/FhirPathTests.cs
@@ -81,5 +81,60 @@ namespace Hl7.Fhir.Support.Tests
             yield return new object[] { poco.ToTypedElement(ModelInspector.Base), "poco to TypedElement" };
 
         }
+
+        public static IEnumerable<object[]> LowBoundaryTestCases() =>
+            new (string expression, bool expected)[]
+                {
+                    ("1.587.lowBoundary(8) = 1.58650000", true),
+                    ("1.587.lowBoundary()  = 1.5865", true),
+                    ("0.0.lowBoundary()    = -0.05", true),
+                    ("1.1.lowBoundary(8)   = 1.05000000", true),
+                    ("1.25.lowBoundary(8)  = 1.24500000", true),
+                    ("1.0.lowBoundary(8)   = 0.95000000", true),
+                    ("2400.lowBoundary(8)  = 2399.50000", true),
+                    ("2.25.lowBoundary(8)  = 2.24500000", true),
+                    ("2.2500.lowBoundary(8)= 2.24995000", true),
+                    ("1.587 'cm'.lowBoundary(8) = 1.58650000 'cm'", true),
+                    ("@2014.lowBoundary(6) = @2014-01", true),
+                    ("@2014.lowBoundary() = @2014-01", true),
+                    ("@2014.lowBoundary(8) = @2014-01-01", true),
+                    ("@2014-01-01T08.lowBoundary(17) = @2014-01-01T08:00:00.000", true),
+                    ("@T10:30.lowBoundary(9) = @T10:30:00.0001", true),
+                    ("@T10:30.lowBoundary() = @T10:30:00", true),
+                }.Select(t => new object[] { t.expression, t.expected });
+
+        public static IEnumerable<object[]> HighBoundaryTestCases() =>
+            new (string expression, bool expected)[]
+                {
+                    ("1.587.highBoundary(8) = 1.58750000", true),
+                    ("1.587.highBoundary()  = 1.5875", true),
+                    ("0.0.highBoundary()    = 0.05", true),
+                    ("1.1.highBoundary(8)   = 1.15000000", true),
+                    ("1.25.highBoundary(8)  = 1.25500000", true),
+                    ("1.0.highBoundary(8)   = 1.05000000", true),
+                    ("2400.highBoundary(8)  = 2400.50000", true),
+                    ("2.25.highBoundary(8)  = 2.2550000", true),
+                    ("2.2500.highBoundary(8)= 2.25005000", true),
+                    ("1.587 'cm'.highBoundary(8) = 1.58750000 'cm'", true),
+                    ("@2014.highBoundary(6) = @2014-12", true),
+                    ("@2014.highBoundary(8) = @2014-12-31", true),
+                    ("@T10:30.highBoundary(9) = @T10:30:59.999", true),
+                    ("@T10:30.highBoundary() = @T10:30:59.999", true),
+
+                }.Select(t => new object[] { t.expression, t.expected });
+
+        public static IEnumerable<object[]> AllTestCases() =>
+            LowBoundaryTestCases().Concat(HighBoundaryTestCases());
+
+
+        [DataTestMethod]
+        [DynamicData(nameof(AllTestCases), DynamicDataSourceType.Method)]
+        public void AssertTestcases(string expression, bool? expected, bool invalid = false)
+        {
+            var evaluator = _compiler.Compile(expression);
+            var result = evaluator(null, FhirEvaluationContext.CreateDefault());
+
+            result.Single().Value.Should().Be(expected);
+        }
     }
 }
