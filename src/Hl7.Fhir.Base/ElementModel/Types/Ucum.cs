541246897f72b89963b772652d3297647dd0ffac
Marco Visser <marco@fire.ly>
2023-05-08 13:36:39 +0200

    First attempt

---
+++ src/Hl7.Fhir.Base/ElementModel/Types/Ucum.cs
@@ -0,0 +1,29 @@
+﻿/* 
+ * Copyright (c) 2019, Firely (info@fire.ly) and contributors
+ * See the file CONTRIBUTORS for details.
+ * 
+ * This file is licensed under the BSD 3-Clause license
+ * available at https://raw.githubusercontent.com/FirelyTeam/firely-net-sdk/master/LICENSE
+ */
+
+#nullable enable
+
+
+using Fhir.Metrics;
+using Hl7.Fhir.Utility;
+using P = Hl7.Fhir.ElementModel.Types;
+
+namespace Hl7.Fhir.Base.ElementModel
+{
+    internal static class Ucum
+    {
+        private static readonly SystemOfUnits SYSTEM = UCUM.Load();
+
+        public static Result<int> Compare(P.Quantity quantity, P.Quantity other)
+        {
+            return 0;
+        }
+    }
+}
+
+#nullable restore
---
+++ src/Hl7.Fhir.Support.Tests/ElementModel/UcumTests.cs
@@ -0,0 +1,44 @@
+﻿using FluentAssertions;
+using Hl7.Fhir.Base.ElementModel;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using P = Hl7.Fhir.ElementModel.Types;
+namespace Hl7.Fhir.Support.Tests.ElementModel
+{
+    [TestClass]
+    public class UcumTests
+    {
+        public enum MyEnum
+        {
+            smallerThan,
+            Equals,
+            greaterThan
+        }
+
+        [DataTestMethod]
+        [DataRow(1.0f, "m", 1.0f, "m", MyEnum.Equals)]
+        //[DataRow(1.0f, "m", 1.0f, "in", MyEnum.greaterThan)]
+        //[DataRow(1.0f, "m", 1.0f, "in", MyEnum.greaterThan)]
+        //[DataRow(1.0f, "m", 1.0f, "in", MyEnum.greaterThan)]
+        public void MyTestMethod(decimal dLeft, string unitLeft, decimal dRight, string unitRight, MyEnum expectedResult)
+        {
+
+            var result = Ucum.Compare(new P.Quantity(dLeft, unitLeft), new P.Quantity(dRight, unitRight));
+
+            result.Success.Should().BeTrue();
+            switch (expectedResult)
+            {
+                case MyEnum.smallerThan:
+                    result.ValueOrDefault().Should().BeNegative();
+                    break;
+                case MyEnum.Equals:
+                    result.ValueOrDefault().Should().Be(0);
+                    break;
+                case MyEnum.greaterThan:
+                    result.ValueOrDefault().Should().BePositive();
+                    break;
+            }
+
+        }
+
+    }
+}
