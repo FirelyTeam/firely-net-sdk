768e59dd606e8ec6d006b1abbc67d1b5fc678bbb
Scott Favre <favre@careevolution.com>
2023-06-05 13:43:07 -0400

    remove the `'` from custom date/time format strings
    TryParseExact uses a stringbulider for each delimiter literal when it is surrounded by single quotes, which can end up with a lot of strings on the heap.
    
    Since we are specifying the invariant culture for each call, the `'` are not needed and we can just use the unescaped delimiter characters.

---
+++ src/Hl7.Fhir.Core.Tests/TestMethodWithCultureAttribute.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Globalization;
+
+namespace Hl7.Fhir.Core.Tests
+{
+    public class TestMethodWithCultureAttribute : TestMethodAttribute
+    {
+        private readonly CultureInfo _culture;
+
+        public TestMethodWithCultureAttribute(string culture)
+        {
+            _culture = new CultureInfo(culture, false);
+        }
+
+        public override TestResult[] Execute(ITestMethod testMethod)
+        {
+            var originalCulture = System.Threading.Thread.CurrentThread.CurrentCulture;
+            var originalUICulture = System.Threading.Thread.CurrentThread.CurrentUICulture;
+            try
+            {
+                System.Threading.Thread.CurrentThread.CurrentCulture = _culture;
+                System.Threading.Thread.CurrentThread.CurrentUICulture = _culture;
+
+                return base.Execute(testMethod);
+            }
+            finally
+            {
+                System.Threading.Thread.CurrentThread.CurrentCulture = originalCulture;
+                System.Threading.Thread.CurrentThread.CurrentUICulture = originalUICulture;
+            }
+        }
+    }
+}
