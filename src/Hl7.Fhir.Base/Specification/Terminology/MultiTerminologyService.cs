7560c097e54b903a278cb8048f3159acc58fd7e3
Marten Smits <m.smits@furore.com>
2023-07-13 17:30:02 +0200

    feature: adding routing mechanism to the MultiTerminologyService

--- src/Hl7.Fhir.Base/Specification/Terminology/MultiTerminologyService.cs
+++ src/Hl7.Fhir.Base/Specification/Terminology/MultiTerminologyService.cs
@@ -11,20 +11,39 @@ using System.Threading.Tasks;
 namespace Hl7.Fhir.Specification.Terminology
 {
     /// <summary>
+    /// A <see cref="ITerminologyService"/> together with the corresponding options, used for ordening.
+    /// </summary>
+    /// <param name="Settings">Settings needed for ordening.</param>
+    /// <param name="Service">A <see cref="ITerminologyService"/>.</param>
+    public record OrderableTerminologyService(ITerminologyService Service, TerminologyServiceSettings Settings)
+    {
+
+    }
+
+    /// <summary>
     /// Aggregation of multiple terminology services. 
     /// </summary>
     public class MultiTerminologyService : ITerminologyService
     {
-        private readonly List<ITerminologyService> _services = new();
+        private readonly List<OrderableTerminologyService> _termServices = new();
 
         /// <summary>
         /// A collection of multiple terminology services to allow for one or multiple fallback services when validating codes for example.
         /// </summary>
         /// <param name="services">Terminology services to be used. Note that the first service in the list will be used first, the others will be used for fallback based on their order.</param>
-        public MultiTerminologyService(IEnumerable<ITerminologyService> services)
+        public MultiTerminologyService(IEnumerable<ITerminologyService> services) : this(services.Select(s => new OrderableTerminologyService(s, TerminologyServiceSettings.CreateDefault())))
+        {
+
+        }
+
+        /// <summary>
+        /// A collection of multiple terminology services to allow for one or multiple fallback services when validating codes for example.
+        /// </summary>
+        /// <param name="services">Orderable terminology services to be used. You can set the order of the services to be used using the settings, the others will be used for fallback based on their order.</param>
+        public MultiTerminologyService(IEnumerable<OrderableTerminologyService> services)
         {
             if (services == null) throw Error.ArgumentNull(nameof(services));
-            _services.AddRange(services);
+            _termServices = services.OrderBy(s => s.Settings.Order).ToList();
         }
 
         public MultiTerminologyService(params ITerminologyService[] services) : this((IEnumerable<ITerminologyService>)services) { }
@@ -52,11 +71,18 @@ namespace Hl7.Fhir.Specification.Terminology
         {
             List<FhirOperationException> exceptions = new();
 
-            foreach (var service in _services)
+            // check if any preferedServices
+            var preferred = preferredService(parameters);
+            var orderedTermServices = preferred.Any() ?
+                                        reorderList(_termServices.Select(t => t.Service), preferred)
+                                        : _termServices.Select(t => t.Service);
+
+            // then check other services
+            foreach (var termService in orderedTermServices)
             {
                 try
                 {
-                    return await service.ValueSetValidateCode(parameters, id, useGet).ConfigureAwait(false);
+                    return await termService.ValueSetValidateCode(parameters, id, useGet).ConfigureAwait(false);
                 }
                 catch (FhirOperationException ex)
                 {
@@ -77,6 +103,20 @@ namespace Hl7.Fhir.Specification.Terminology
             }
 
             throw new InvalidOperationException("We should never have come here");
+        }
+
+        private IEnumerable<ITerminologyService> preferredService(Parameters parameters)
+        {
+            var valueSetUrl = new ValidateCodeParameters(parameters).Url.Value;
+            return _termServices.Where(t => matchSystem(t.Settings.PreferredValueSets, valueSetUrl)).Select(t => t.Service);
+
+            static bool matchSystem(IEnumerable<string> preferredSystems, string valueSetUrl) =>
+                !preferredSystems.Any() || preferredSystems.Any(valueSetUrl.StartsWith);
+        }
+
+        private static IEnumerable<T> reorderList<T>(IEnumerable<T> originalList, IEnumerable<T> itemsToMove)
+        {
+            return itemsToMove.Concat(originalList.Except(itemsToMove));
         }
     }
 }
