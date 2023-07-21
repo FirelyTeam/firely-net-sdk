1b4de640957d9fa0a9f531914e1f133e5675ef08
Marten Smits <m.smits@furore.com>
2023-07-21 13:37:55 +0200

    feature: create factory methods for "core" terminology service

---
+++ src/Hl7.Fhir.STU3/Specification/Terminology/TerminologyServiceFactory.cs
@@ -0,0 +1,39 @@
+﻿#nullable enable
+
+using Hl7.Fhir.Specification.Source;
+
+namespace Hl7.Fhir.Specification.Terminology
+{
+    public static class TerminologyServiceFactory
+    {
+        /// <summary>
+        /// Creates a MultiTerminologyService, which combines a LocalTerminologyService to retrieve the core FHIR resources with custom services to validate some implicit core ValueSets.
+        /// </summary>
+        /// <param name="coreResourceResolver">Resource resolves to resolve FHIR core artifacts</param>
+        /// <param name="expanderSettings">ValueSet expansion settings</param>
+        /// <returns>A MultiTerminologyService, which combines a LocalTerminologyService to retrieve the core FHIR resources with custom services to validate some implicit core ValueSets</returns>
+        public static MultiTerminologyService CreateDefaultForCore(IAsyncResourceResolver coreResourceResolver, ValueSetExpanderSettings? expanderSettings = null)
+        {
+            var mimeTypeRoutingSettings = new TerminologyServiceRoutingSettings(new MimeTypeTerminologyService())
+            {
+                PreferredValueSets = new string[]
+                {
+                    "http://www.rfc-editor.org/bcp/bcp13.txt"
+                }
+            };
+
+            var localTermRoutingSettings = new TerminologyServiceRoutingSettings(new LocalTerminologyService(coreResourceResolver, expanderSettings))
+            {
+                PreferredValueSets = new string[]
+                {
+                    "http://hl7.org/fhir/ValueSet/"
+                }
+            };
+
+            return new MultiTerminologyService(mimeTypeRoutingSettings, localTermRoutingSettings);
+        }
+
+    }
+}
+
+#nullable restore
