6f51919046d4c8c29a12ee414663fbe9dcbc7101
Ewout Kramer <ewout@fire.ly>
2024-01-17 22:45:55 +0100

    WIP

---
+++ src/Hl7.Fhir.Base/FhirPath/Dynamic/FhirPocoDynamicMetaObject.cs
@@ -0,0 +1,20 @@
+﻿/* 
+ * Copyright (c) 2024, Firely (info@fire.ly) and contributors
+ * See the file CONTRIBUTORS for details.
+ * 
+ * This file is licensed under the BSD 3-Clause license
+ * available at https://raw.githubusercontent.com/FirelyTeam/firely-net-sdk/master/LICENSE
+ */
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Hl7.Fhir.Base.FhirPath.Dynamic
+{
+    internal class FhirPocoDynamicMetaObject
+    {
+    }
+}
