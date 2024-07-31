cd5ef24e69410965b5c97fb342fa7723a98df7b2
Kasdejong <k.j2.de.jong@student.vu.nl>
2024-07-31 09:57:17 +0200

    added OverrideReason.cs

--- src/Hl7.Fhir.Base/Model/CdsHooks/OverrideReason.cs
+++ src/Hl7.Fhir.Base/Model/CdsHooks/OverrideReason.cs
@@ -1,6 +1,9 @@
+#nullable enable
+
 namespace Hl7.Fhir.Model.CdsHooks;
 
 public class OverrideReason
 {
-    
+    public Coding? Reason { get; set; }
+    public string? UserComment { get; set; }
 }
