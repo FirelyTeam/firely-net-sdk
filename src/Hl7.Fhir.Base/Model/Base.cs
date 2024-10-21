48b7f7ba25af9aac435a04baa2e91d3d83c801f4
Kasdejong <k.j2.de.jong@student.vu.nl>
2024-10-21 09:27:52 +0200

    removed unused imports after merge

--- src/Hl7.Fhir.Base/Model/Base.cs
+++ src/Hl7.Fhir.Base/Model/Base.cs
@@ -37,9 +37,7 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.ComponentModel.DataAnnotations;
 using System.Diagnostics.CodeAnalysis;
-using System.Linq;
 using System.Threading;
-using DataType = System.ComponentModel.DataAnnotations.DataType;
 
 namespace Hl7.Fhir.Model;
 
