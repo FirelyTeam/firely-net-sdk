5946ff67b4f4b4cdedb1de83a47e91315cbaf4cf
mmsmits <mmsmits@users.noreply.github.com>
2024-11-20 14:45:20 +0100

    Revert "Merge pull request #2943 from FirelyTeam/feature/use-proper-snapshot-generation-settings"
    This reverts commit a4230b73594cd106e77cb3b36de49da775995c4c, reversing
    changes made to 962f40875436a9f58ae6c2425773c2d19013f9f5.

--- src/Hl7.Fhir.Shims.Base/Specification/Source/SnapshotSource.cs
+++ src/Hl7.Fhir.Shims.Base/Specification/Source/SnapshotSource.cs
@@ -98,17 +98,14 @@ namespace Hl7.Fhir.Specification.Source
         {
             if (res is StructureDefinition sd)
             {
-                var shouldGenerate = Generator.Settings.RegenerationBehaviour switch
-                {
-                    RegenerationSettings.TRY_USE_EXISTING => !sd.HasSnapshot,
-                    RegenerationSettings.REGENERATE_ONCE => !sd.HasSnapshot || !sd.Snapshot.IsCreatedBySnapshotGenerator(),
-#pragma warning disable CS0618 // Type or member is obsolete
-                    RegenerationSettings.FORCE_REGENERATE => true,
-#pragma warning restore CS0618 // Type or member is obsolete
-                    _ => throw Error.NotSupported($"Unknown regeneration behaviour: {Generator.Settings.RegenerationBehaviour}")
-                };
-                
-                if (shouldGenerate)
+                if (
+                    !sd.HasSnapshot || 
+                    Generator.Settings.RegenerationBehaviour == RegenerationSettings.FORCE_REGENERATE || 
+                    (
+                        !sd.Snapshot.IsCreatedBySnapshotGenerator() && 
+                        Generator.Settings.RegenerationBehaviour == RegenerationSettings.REGENERATE_ONCE
+                    )
+                )
                 {
                     await Generator.UpdateAsync(sd).ConfigureAwait(false);
                 }
