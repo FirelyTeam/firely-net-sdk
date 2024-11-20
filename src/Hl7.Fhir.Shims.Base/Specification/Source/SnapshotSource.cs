08c5f1f774efdf8db26e97ee18347af2272b1641
mmsmits <mmsmits@users.noreply.github.com>
2024-11-20 14:53:53 +0100

    Revert "Merge pull request #2803 from FirelyTeam/fix/snapshotsource-reuse-snapshot"
    This reverts commit 846cdf559916098a41936f62ba3595911bad92f4, reversing
    changes made to f8ef048aa556c13e5c431053d7581dd447ffc0d5.

--- src/Hl7.Fhir.Shims.Base/Specification/Source/SnapshotSource.cs
+++ src/Hl7.Fhir.Shims.Base/Specification/Source/SnapshotSource.cs
@@ -54,9 +54,7 @@ namespace Hl7.Fhir.Specification.Source
         private static SnapshotGeneratorSettings createSettings(bool regenerate)
         {
             var settings = SnapshotGeneratorSettings.CreateDefault();
-#pragma warning disable CS0618 // Type or member is obsolete
             settings.ForceRegenerateSnapshots = regenerate;
-#pragma warning restore CS0618 // Type or member is obsolete
             return settings;
         }
 
@@ -72,11 +70,11 @@ namespace Hl7.Fhir.Specification.Source
 
         #region IResourceResolver
 
-        private IAsyncResourceResolver Resolver => Generator.AsyncResolver;
+        private IAsyncResourceResolver _resolver => Generator.AsyncResolver;
 
         /// <summary>Find a resource based on its relative or absolute uri.</summary>
         /// <remarks>The source ensures that resolved <see cref="StructureDefinition"/> instances have a snapshot component.</remarks>
-        public async Tasks.Task<Resource> ResolveByUriAsync(string uri) => await ensureSnapshot(await Resolver.ResolveByUriAsync(uri).ConfigureAwait(false)).ConfigureAwait(false);
+        public async Tasks.Task<Resource> ResolveByUriAsync(string uri) => await ensureSnapshot(await _resolver.ResolveByUriAsync(uri).ConfigureAwait(false)).ConfigureAwait(false);
 
         /// <inheritdoc cref="ResolveByUriAsync(string)"/>
         [Obsolete("SnapshotSource now works best with asynchronous resolvers. Use ResolveByUriAsync() instead.")]
@@ -84,7 +82,7 @@ namespace Hl7.Fhir.Specification.Source
 
         /// <summary>Find a (conformance) resource based on its canonical uri.</summary>
         /// <remarks>The source ensures that resolved <see cref="StructureDefinition"/> instances have a snapshot component.</remarks>
-        public async Tasks.Task<Resource> ResolveByCanonicalUriAsync(string uri) => await ensureSnapshot(await Resolver.ResolveByCanonicalUriAsync(uri).ConfigureAwait(false)).ConfigureAwait(false);
+        public async Tasks.Task<Resource> ResolveByCanonicalUriAsync(string uri) => await ensureSnapshot(await _resolver.ResolveByCanonicalUriAsync(uri).ConfigureAwait(false)).ConfigureAwait(false);
 
         /// <inheritdoc cref="ResolveByCanonicalUriAsync(string)"/>
         [Obsolete("SnapshotSource now works best with asynchronous resolvers. Use ResolveByCanonicalUriAsync() instead.")]
@@ -98,14 +96,7 @@ namespace Hl7.Fhir.Specification.Source
         {
             if (res is StructureDefinition sd)
             {
-                if (
-                    !sd.HasSnapshot || 
-                    Generator.Settings.RegenerationBehaviour == RegenerationSettings.FORCE_REGENERATE || 
-                    (
-                        !sd.Snapshot.IsCreatedBySnapshotGenerator() && 
-                        Generator.Settings.RegenerationBehaviour == RegenerationSettings.REGENERATE_ONCE
-                    )
-                )
+                if (!sd.HasSnapshot || Generator.Settings.ForceRegenerateSnapshots || !sd.Snapshot.IsCreatedBySnapshotGenerator())
                 {
                     await Generator.UpdateAsync(sd).ConfigureAwait(false);
                 }
@@ -116,6 +107,6 @@ namespace Hl7.Fhir.Specification.Source
         // Allow derived classes to override
         // http://blogs.msdn.com/b/jaredpar/archive/2011/03/18/debuggerdisplay-attribute-best-practices.aspx
         [DebuggerBrowsable(DebuggerBrowsableState.Never)]
-        internal protected virtual string DebuggerDisplay => $"{GetType().Name} for {Resolver.DebuggerDisplayString()}";
+        internal protected virtual string DebuggerDisplay => $"{GetType().Name} for {_resolver.DebuggerDisplayString()}";
     }
 }
