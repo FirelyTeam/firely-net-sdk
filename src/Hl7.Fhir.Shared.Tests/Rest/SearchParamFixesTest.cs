ebc733a36e39d47d1642f3f67be49da776837d18
Ewout Kramer <ewout@fire.ly>
2024-12-03 19:27:22 +0100

    Moved SearchParamDefinition from STU3+ to a separate class in Base.

--- src/Hl7.Fhir.Shared.Tests/Rest/SearchParamFixesTest.cs
+++ src/Hl7.Fhir.Shared.Tests/Rest/SearchParamFixesTest.cs
@@ -43,23 +43,23 @@ namespace Hl7.Fhir.Test.Rest
 
 
         [TestMethod]
-        [DataRow("DiagnosticReport", "encounter", ResourceType.EpisodeOfCare, "05bfc4f1d0a4568ca405e248c055a8a16d857ffb")]
-        [DataRow("RiskAssessment", "encounter", ResourceType.EpisodeOfCare, "3b071d478ff3cb744cb6668ac8512dc7362e6737")]
-        [DataRow("List", "encounter", ResourceType.EpisodeOfCare, "3b071d478ff3cb744cb6668ac8512dc7362e6737")]
-        [DataRow("VisionPrescription", "encounter", ResourceType.EpisodeOfCare, "3b071d478ff3cb744cb6668ac8512dc7362e6737")]
-        [DataRow("ServiceRequest", "encounter", ResourceType.EpisodeOfCare, "3b071d478ff3cb744cb6668ac8512dc7362e6737")]
-        [DataRow("Flag", "encounter", ResourceType.EpisodeOfCare, "3b071d478ff3cb744cb6668ac8512dc7362e6737")]
-        [DataRow("Observation", "encounter", ResourceType.EpisodeOfCare, "3b071d478ff3cb744cb6668ac8512dc7362e6737")]
-        [DataRow("NutritionOrder", "encounter", ResourceType.EpisodeOfCare, "3b071d478ff3cb744cb6668ac8512dc7362e6737")]
-        [DataRow("Composition", "encounter", ResourceType.EpisodeOfCare, "3b071d478ff3cb744cb6668ac8512dc7362e6737")]
-        [DataRow("DeviceRequest", "encounter", ResourceType.EpisodeOfCare, "3b071d478ff3cb744cb6668ac8512dc7362e6737")]
-        [DataRow("Procedure", "encounter", ResourceType.EpisodeOfCare, "3b071d478ff3cb744cb6668ac8512dc7362e6737")]
-        public void CheckManualFixesOfTemplateModelInfo(string resource, string spName, ResourceType targetResource, string commit)
+        [DataRow("DiagnosticReport", "encounter", VersionIndependentResourceTypesAll.EpisodeOfCare, "05bfc4f1d0a4568ca405e248c055a8a16d857ffb")]
+        [DataRow("RiskAssessment", "encounter", VersionIndependentResourceTypesAll.EpisodeOfCare, "3b071d478ff3cb744cb6668ac8512dc7362e6737")]
+        [DataRow("List", "encounter", VersionIndependentResourceTypesAll.EpisodeOfCare, "3b071d478ff3cb744cb6668ac8512dc7362e6737")]
+        [DataRow("VisionPrescription", "encounter", VersionIndependentResourceTypesAll.EpisodeOfCare, "3b071d478ff3cb744cb6668ac8512dc7362e6737")]
+        [DataRow("ServiceRequest", "encounter", VersionIndependentResourceTypesAll.EpisodeOfCare, "3b071d478ff3cb744cb6668ac8512dc7362e6737")]
+        [DataRow("Flag", "encounter", VersionIndependentResourceTypesAll.EpisodeOfCare, "3b071d478ff3cb744cb6668ac8512dc7362e6737")]
+        [DataRow("Observation", "encounter", VersionIndependentResourceTypesAll.EpisodeOfCare, "3b071d478ff3cb744cb6668ac8512dc7362e6737")]
+        [DataRow("NutritionOrder", "encounter", VersionIndependentResourceTypesAll.EpisodeOfCare, "3b071d478ff3cb744cb6668ac8512dc7362e6737")]
+        [DataRow("Composition", "encounter", VersionIndependentResourceTypesAll.EpisodeOfCare, "3b071d478ff3cb744cb6668ac8512dc7362e6737")]
+        [DataRow("DeviceRequest", "encounter", VersionIndependentResourceTypesAll.EpisodeOfCare, "3b071d478ff3cb744cb6668ac8512dc7362e6737")]
+        [DataRow("Procedure", "encounter", VersionIndependentResourceTypesAll.EpisodeOfCare, "3b071d478ff3cb744cb6668ac8512dc7362e6737")]
+        public void CheckManualFixesOfTemplateModelInfo(string resource, string spName, VersionIndependentResourceTypesAll targetResource, string commit)
         {
-            var sp = ModelInfo.SearchParameters.Where(s => s.Resource == resource && s.Name == spName).FirstOrDefault();
+            var sp = ModelInfo.SearchParameters.FirstOrDefault(s => s.Resource == resource && s.Name == spName);
             if (sp is not null)
             {
-                Assert.IsFalse(sp.Target.Contains(targetResource),
+                Assert.IsFalse(sp.Target?.Contains(targetResource),
                     $"Manualy removed target {targetResource} from searchparameter {resource}.{spName}. Commit: {commit}");
             }
         }
