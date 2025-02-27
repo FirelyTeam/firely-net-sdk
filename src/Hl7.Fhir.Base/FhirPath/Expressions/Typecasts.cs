727551d39b209f086b028101986318f3098f3862
Kasdejong <k.j2.de.jong@student.vu.nl>
2025-02-27 16:02:21 +0100

    wip. note that functionsTests is temp changed

--- src/Hl7.Fhir.Base/FhirPath/Expressions/Typecasts.cs
+++ src/Hl7.Fhir.Base/FhirPath/Expressions/Typecasts.cs
@@ -59,7 +59,7 @@ namespace Hl7.FhirPath.Expressions
 
             // this check seems weird, but PocoElementNode both implements PocoNode and IEnumerable<PocoNode> for the sake of backwards compatibility
             bool fromElemList = from.CanBeTreatedAsType(typeof(IEnumerable<PocoNode>)) && !from.CanBeTreatedAsType(typeof(PocoNode));
-            if (to.CanBeTreatedAsType(typeof(P.Any)) && from.CanBeTreatedAsType(typeof(PocoNode))) return tryQuantity;
+            if (to.CanBeTreatedAsType(typeof(P.Quantity)) && from.CanBeTreatedAsType(typeof(PocoNode))) return tryQuantity;
             if (to == typeof(PocoNode) && !fromElemList) return any2primitiveTypedElement;
             if (to == typeof(IEnumerable<PocoNode>)) return any2SingleItemList;
 
@@ -75,8 +75,14 @@ namespace Hl7.FhirPath.Expressions
 
             if (typeof(P.Any).IsAssignableFrom(to) && !fromElemList)
             {
-                if (f is PocoNode {Poco: Quantity} q) 
-                    return _ => q;
+                if (f is PrimitiveNode { Value: null })
+                    return _ => null;
+                
+                if (f is PocoNode {Poco: P.IToSystemPrimitive tsp} && tsp.TryConvertToSystemType(out var result))
+                {
+                    return _ => result;
+                }
+
                 return o => P.Any.Convert(o);
             }
 
