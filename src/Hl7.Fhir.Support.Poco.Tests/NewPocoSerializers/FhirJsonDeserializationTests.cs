302f8145afdaece2558e400f9b90eb46536f638f
Ewout Kramer <ewout@fire.ly>
2025-04-04 10:34:11 +0200

    With the output file included as artifacts, we don't need fancy test paths.

--- src/Hl7.Fhir.Support.Poco.Tests/NewPocoSerializers/FhirJsonDeserializationTests.cs
+++ src/Hl7.Fhir.Support.Poco.Tests/NewPocoSerializers/FhirJsonDeserializationTests.cs
@@ -495,7 +495,7 @@ public class FhirJsonDeserializationTests
 
     [TestMethod]
     [Ignore]
-    public void OverwriteTestDataForRecoveryTest() => testRecovery(true, "../../../TestData");
+    public void OverwriteTestDataForRecoveryTest() => testRecovery(true, "TestData");
     
     /// fileDir is a fix for the test framework only editing temp files.
     private void testRecovery(bool overwrite, string fileDir)
@@ -508,7 +508,7 @@ public class FhirJsonDeserializationTests
 
         try
         {
-            var _ = JsonSerializer.Deserialize<Patient>(jsonInput, options);
+            _ = JsonSerializer.Deserialize<Patient>(jsonInput, options);
             Assert.Fail("Should have encountered errors.");
         }
         catch (DeserializationFailedException dfe)
