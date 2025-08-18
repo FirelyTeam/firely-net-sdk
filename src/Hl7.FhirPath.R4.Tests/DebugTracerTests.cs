31d12eac170e5caa18bfabf6676c4bb692a9a28a
brian_pos <brian_pos@hotmail.com>
2025-08-18 16:54:28 +1000

    Include assertions into the debug trace tests too.

--- src/Hl7.FhirPath.R4.Tests/DebugTracerTests.cs
+++ src/Hl7.FhirPath.R4.Tests/DebugTracerTests.cs
@@ -39,7 +39,20 @@ namespace Hl7.FhirPath.Tests
         private class TestDebugTracer: IDebugTracer
         {
             public List<string> traceOutput = new List<string>();
-            public List<ExceptionDispatchInfo> exceptions = new List<ExceptionDispatchInfo>();
+            private List<ExceptionDispatchInfo> exceptions = new List<ExceptionDispatchInfo>();
+
+            public void Assert()
+            {
+                if (exceptions.Count == 0)
+                    return; // no exceptions to throw
+                System.Diagnostics.Trace.WriteLine($"Tracer exceptions: {exceptions.Count}");
+                foreach (var item in exceptions)
+                {
+                    item.Throw();
+                }
+            }
+
+
             public void TraceCall(
                 Expression expr,
                 int contextId,
@@ -120,22 +133,22 @@ namespace Hl7.FhirPath.Tests
 
             public void DumpDiagnostics()
             {
+                System.Diagnostics.Trace.WriteLine("---");
                 foreach (var item in traceOutput)
                 {
                     System.Diagnostics.Trace.WriteLine(item);
                 }
             }
 
-            public string DebugTraceValue(Expression expr, ITypedElement? item)
+            public string DebugTraceValue(ITypedElement? item)
             {
-                string exprName = TraceExpressionNodeName(expr);
                 if (item == null)
                     return null; // possible with a null focus to kick things off
 
                 if (item.Location == "@primitivevalue@" || item.Location == "@QuantityAsPrimitiveValue@")
-                    return $"{exprName}:\t{item.Value}\t({item.InstanceType})";
+                    return $"{item.Value}\t({item.InstanceType})";
 
-                return $"{exprName}:\t{item.Value}\t({item.InstanceType})\t{item.Location}";
+                return $"{item.Value}\t({item.InstanceType})\t{item.Location}";
             }
         }
 
@@ -145,9 +158,42 @@ namespace Hl7.FhirPath.Tests
             var expression = "Patient.birthDate.toString().substring(0, 4)";
             var input = fixture.PatientExample.ToTypedElement().ToScopedNode();
             var tracer = new TestDebugTracer();
+            tracer.TraceNode = (n, expr, contextId, focus, thisValue, index, totalValue, result) =>
+            {
+                DiagnosticsDebugTracer.DebugTraceCall(expr, contextId, focus, thisValue, index, totalValue, result, null);
+                var vThis = tracer.DebugTraceValue(thisValue?.FirstOrDefault());
+                Assert.AreEqual("\t(Patient)\tPatient", vThis);
+                var vFocus = tracer.DebugTraceValue(focus?.FirstOrDefault());
+                var vResult = tracer.DebugTraceValue(result?.FirstOrDefault());
+
+                if (n == 2)
+                {
+                    // toString
+                    Assert.AreEqual("1974-12-25\t(date)\tPatient.birthDate[0]", vFocus);
+                    Assert.AreEqual("1974-12-25\t(System.String)", vResult);
+                }
+                if (n == 3)
+                {
+                    // constant 0
+                    Assert.AreEqual("\t(Patient)\tPatient", vFocus);
+                    Assert.AreEqual("0\t(System.Integer)", vResult);
+                }
+                if (n == 4)
+                {
+                    // constant 4
+                    Assert.AreEqual("\t(Patient)\tPatient", vFocus);
+                    Assert.AreEqual("4\t(System.Integer)", vResult);
+                }
+                if (n == 5)
+                {
+                    // substring
+                    Assert.AreEqual("1974-12-25\t(System.String)", vFocus);
+                    Assert.AreEqual("1974\t(System.String)", vResult);
+                }
+            };
             var expr = compiler.Compile(expression, true);
+            Trace.WriteLine("Expression: " + expression + "\r\n");
             var results = expr(input, new FhirEvaluationContext() { DebugTracer = tracer }).ToFhirValues().ToList();
-            System.Diagnostics.Trace.WriteLine("Expression: " + expression);
             tracer.DumpDiagnostics();
 
             Assert.AreEqual(1, results.Count());
@@ -160,9 +206,55 @@ namespace Hl7.FhirPath.Tests
             Assert.AreEqual("39,1,constant: focus=1 result=1", tracer.traceOutput[3]);
             Assert.AreEqual("42,1,constant: focus=1 result=1", tracer.traceOutput[4]);
             Assert.AreEqual("29,9,substring: focus=1 result=1", tracer.traceOutput[5]);
+
+            // Now check the tracer assertions
+            tracer.Assert();
         }
 
         [TestMethod]
+        public void testDebugTrace_PropertyAndFunctionCalls()
+        {
+            var expression = "Patient.id.indexOf('am')";
+            var input = fixture.PatientExample.ToTypedElement().ToScopedNode();
+            var tracer = new TestDebugTracer();
+            tracer.TraceNode = (n, expr, contextId, focus, thisValue, index, totalValue, result) =>
+            {
+                DiagnosticsDebugTracer.DebugTraceCall(expr, contextId, focus, thisValue, index, totalValue, result, null);
+                var vThis = tracer.DebugTraceValue(thisValue?.FirstOrDefault());
+                var vFocus = tracer.DebugTraceValue(focus?.FirstOrDefault());
+                var vResult = tracer.DebugTraceValue(result?.FirstOrDefault());
+                Assert.AreEqual("\t(Patient)\tPatient", vThis); // in this specific expression, this is always the patient
+                if (n == 2)
+                {
+                    // the context and results of the constant 'am' call
+                    Assert.AreEqual("\t(Patient)\tPatient", vFocus);
+                    Assert.AreEqual("am\t(System.String)", vResult);
+                }
+                if (n == 3)
+                {
+                    // the context and results of toString call
+                    Assert.AreEqual("example\t(id)\tPatient.id[0]", vFocus);
+                    Assert.AreEqual("2\t(System.Integer)", vResult);
+                }
+            };
+            var expr = compiler.Compile(expression, true);
+            Trace.WriteLine("Expression: " + expression + "\r\n");
+            var results = expr(input, new FhirEvaluationContext() { DebugTracer = tracer }).ToFhirValues().ToList();
+            tracer.DumpDiagnostics();
+
+            Assert.AreEqual(1, results.Count());
+            Assert.AreEqual("2", results[0].ToString());
+
+            Assert.AreEqual(4, tracer.traceOutput.Count());
+            Assert.AreEqual("0,7,Patient: focus=1 result=1", tracer.traceOutput[0]);
+            Assert.AreEqual("8,2,id: focus=1 result=1", tracer.traceOutput[1]);
+            Assert.AreEqual("19,4,constant: focus=1 result=1", tracer.traceOutput[2]);
+            Assert.AreEqual("11,7,indexOf: focus=1 result=1", tracer.traceOutput[3]);
+
+            // Now check the tracer assertions
+            tracer.Assert();
+        }
+
         [TestMethod]
         public void testDebugTrace_Aggregate()
         {
@@ -176,23 +268,23 @@ namespace Hl7.FhirPath.Tests
                 {
                     // the results of the | operator
                     DiagnosticsDebugTracer.DebugTraceCall(expr, contextId, focus, thisValue, index, totalValue, result, null);
-                    var vThis = tracer.DebugTraceValue(expr, thisValue?.FirstOrDefault());
-                    var vFocus = tracer.DebugTraceValue(expr, focus?.FirstOrDefault());
-                    var vResult1 = tracer.DebugTraceValue(expr, result?.FirstOrDefault());
-                    var vResult2 = tracer.DebugTraceValue(expr, result?.Skip(1)?.FirstOrDefault());
+                    var vThis = tracer.DebugTraceValue(thisValue?.FirstOrDefault());
+                    var vFocus = tracer.DebugTraceValue(focus?.FirstOrDefault());
+                    var vResult1 = tracer.DebugTraceValue(result?.FirstOrDefault());
+                    var vResult2 = tracer.DebugTraceValue(result?.Skip(1)?.FirstOrDefault());
                     Assert.AreEqual(0, contextId);
                 }
                 if (n == 3) {
                     // the results of the constant "0" for the init expression
                     DiagnosticsDebugTracer.DebugTraceCall(expr, contextId, focus, thisValue, index, totalValue, result, null);
-                    var v1 = tracer.DebugTraceValue(expr, focus?.FirstOrDefault());
-                    var v2 = tracer.DebugTraceValue(expr, focus?.Skip(1)?.FirstOrDefault());
+                    var v1 = tracer.DebugTraceValue(focus?.FirstOrDefault());
+                    var v2 = tracer.DebugTraceValue(focus?.Skip(1)?.FirstOrDefault());
                     // Assert.AreEqual(3, contextId);
                 }
             };
 
             var expr = compiler.Compile(expression, true);
-            System.Diagnostics.Trace.WriteLine("Expression: " + expression);
+            Trace.WriteLine("Expression: " + expression + "\r\n");
             var results = expr(input, new FhirEvaluationContext() { DebugTracer = tracer }).ToFhirValues().ToList();
             tracer.DumpDiagnostics();
 
@@ -215,21 +307,108 @@ namespace Hl7.FhirPath.Tests
             Assert.AreEqual("6,9,aggregate: focus=2 result=1", tracer.traceOutput[n++]);
 
             // Now check the tracer assertions
-            System.Diagnostics.Trace.WriteLine($"Tracer exceptions: {tracer.exceptions.Count}");
-            foreach (var item in tracer.exceptions)
-            {
-                item.Throw();
-            }
+            tracer.Assert();
         }
+
+        [TestMethod]
+        public void testDebugTrace_Operator()
+        {
+            var expression = "Patient.id.toString() = Patient.id";
+            var input = fixture.PatientExample.ToTypedElement().ToScopedNode();
+            var tracer = new TestDebugTracer();
+            tracer.TraceNode = (n, expr, contextId, focus, thisValue, index, totalValue, result) =>
+            {
+                DiagnosticsDebugTracer.DebugTraceCall(expr, contextId, focus, thisValue, index, totalValue, result, null);
+                var vThis = tracer.DebugTraceValue(thisValue?.FirstOrDefault());
+                Assert.AreEqual("\t(Patient)\tPatient", vThis);
+                var vFocus = tracer.DebugTraceValue(focus?.FirstOrDefault());
+                if (n == 2)
+                {
+                    // the context and results of toString call
+                    var vResult = tracer.DebugTraceValue(result?.FirstOrDefault());
+                    Assert.AreEqual("example\t(id)\tPatient.id[0]", vFocus);
+                    Assert.AreEqual("example\t(System.String)", vResult);
+                }
+            };
+
+            var expr = compiler.Compile(expression, true);
+            Trace.WriteLine("Expression: " + expression + "\r\n");
+            var results = expr(input, new FhirEvaluationContext() { DebugTracer = tracer }).ToFhirValues().ToList();
+            tracer.DumpDiagnostics();
+
+            Assert.AreEqual(1, results.Count());
+            Assert.AreEqual("true", results[0].ToString());
+
+            // Now check the tracer outputs
+            Assert.AreEqual(6, tracer.traceOutput.Count());
+            int n = 0;
+            Assert.AreEqual("0,7,Patient: focus=1 result=1", tracer.traceOutput[n++]);
+            Assert.AreEqual("8,2,id: focus=1 result=1", tracer.traceOutput[n++]);
+            Assert.AreEqual("11,8,toString: focus=1 result=1", tracer.traceOutput[n++]);
+            Assert.AreEqual("24,7,Patient: focus=1 result=1", tracer.traceOutput[n++]);
+            Assert.AreEqual("32,2,id: focus=1 result=1", tracer.traceOutput[n++]);
+            Assert.AreEqual("22,1,=: focus=1 result=1", tracer.traceOutput[n++]);
+
+            // Now check the tracer assertions
+            tracer.Assert();
+        }
+
+        [TestMethod]
         public void testDebugTrace_WhereClause()
         {
             var expression = "name.where(use='official' or use='usual').given";
 
             var input = fixture.PatientExample.ToTypedElement().ToScopedNode();
             var tracer = new TestDebugTracer();
+            tracer.TraceNode = (n, expr, contextId, focus, thisValue, index, totalValue, result) =>
+            {
+                DiagnosticsDebugTracer.DebugTraceCall(expr, contextId, focus, thisValue, index, totalValue, result, null);
+                var vThis = tracer.DebugTraceValue(thisValue?.FirstOrDefault());
+                var vFocus = tracer.DebugTraceValue(focus?.FirstOrDefault());
+                var vResult = tracer.DebugTraceValue(result?.FirstOrDefault());
+                var vIndex= index?.Value;
+                if (n == 0)
+                {
+                    // name
+                    Assert.AreEqual("\t(Patient)\tPatient", vThis);
+                    Assert.AreEqual("\t(Patient)\tPatient", vFocus);
+                    Assert.AreEqual(2, result.Count());
+                }
+
+                if (n == 1 || n == 2 || n == 3 || n == 4)
+                {
+                    Assert.AreEqual("\t(HumanName)\tPatient.name[0]", vThis);
+                    Assert.AreEqual("\t(HumanName)\tPatient.name[0]", vFocus);
+                    Assert.AreEqual(0, vIndex);
+                }
+                if (n >= 5 && n <= 11)
+                {
+                    Assert.AreEqual("\t(HumanName)\tPatient.name[1]", vThis);
+                    Assert.AreEqual("\t(HumanName)\tPatient.name[1]", vFocus);
+                    Assert.AreEqual(1, vIndex);
+                }
+
+                if (n == 12)
+                {
+                    // Where clause
+                    Assert.AreEqual("\t(Patient)\tPatient", vThis);
+                    Assert.AreEqual("\t(HumanName)\tPatient.name[0]", vFocus);
+                    Assert.AreEqual(2, focus.Count());
+                    Assert.AreEqual(2, result.Count());
+                    Assert.AreEqual("\t(HumanName)\tPatient.name[0]", vResult);
+                }
+                if (n == 13)
+                {
+                    // The final given prop navigator
+                    Assert.AreEqual("\t(Patient)\tPatient", vThis);
+                    Assert.AreEqual("\t(HumanName)\tPatient.name[0]", vFocus);
+                    Assert.AreEqual(2, focus.Count());
+                    Assert.AreEqual(3, result.Count());
+                }
+            };
             var expr = compiler.Compile(expression, true);
+            Trace.WriteLine("Expression: " + expression + "\r\n");
             var results = expr(input, new FhirEvaluationContext() { DebugTracer = tracer }).ToList();
-            System.Diagnostics.Trace.WriteLine("Expression: " + expression);
             tracer.DumpDiagnostics();
 
             Assert.AreEqual(3, results.Count());
@@ -256,6 +435,9 @@ namespace Hl7.FhirPath.Tests
             Assert.AreEqual("26,2,or: focus=1 result=1", tracer.traceOutput[11]);
             Assert.AreEqual("5,5,where: focus=2 result=2", tracer.traceOutput[12]);
             Assert.AreEqual("42,5,given: focus=2 result=3", tracer.traceOutput[13]);
+
+            // Now check the tracer assertions
+            tracer.Assert();
         }
 
         [TestMethod]
@@ -265,9 +447,13 @@ namespace Hl7.FhirPath.Tests
 
             var input = fixture.PatientExample.ToTypedElement().ToScopedNode();
             var tracer = new TestDebugTracer();
+            tracer.TraceNode = (n, expr, contextId, focus, thisValue, index, totalValue, result) =>
+            {
+                DiagnosticsDebugTracer.DebugTraceCall(expr, contextId, focus, thisValue, index, totalValue, result, null);
+            };
             var expr = compiler.Compile(expression, true);
+            Trace.WriteLine("Expression: " + expression + "\r\n");
             var results = expr(input, new FhirEvaluationContext() { DebugTracer = tracer }).ToFhirValues().ToList();
-            System.Diagnostics.Trace.WriteLine("Expression: " + expression);
             tracer.DumpDiagnostics();
 
             Assert.AreEqual(1, results.Count());
@@ -275,6 +461,9 @@ namespace Hl7.FhirPath.Tests
 
             Assert.AreEqual(1, tracer.traceOutput.Count());
             Assert.AreEqual("0,4,constant: focus=1 result=1", tracer.traceOutput[0]);
+
+            // Now check the tracer assertions
+            tracer.Assert();
         }
 
         [TestMethod]
@@ -284,9 +473,21 @@ namespace Hl7.FhirPath.Tests
 
             var input = fixture.PatientExample.ToTypedElement().ToScopedNode();
             var tracer = new TestDebugTracer();
+            tracer.TraceNode = (n, expr, contextId, focus, thisValue, index, totalValue, result) =>
+            {
+                DiagnosticsDebugTracer.DebugTraceCall(expr, contextId, focus, thisValue, index, totalValue, result, null);
+
+                // interestingly all the nodes in this expression have the same focus and $this value
+                var vThis = tracer.DebugTraceValue(thisValue?.FirstOrDefault());
+                var vFocus = tracer.DebugTraceValue(focus?.FirstOrDefault());
+                Assert.AreEqual("\t(Patient)\tPatient", vThis);
+                Assert.AreEqual("\t(Patient)\tPatient", vFocus);
+
+
+            };
             var expr = compiler.Compile(expression, true);
+            Trace.WriteLine("Expression: " + expression + "\r\n");
             var results = expr(input, new FhirEvaluationContext() { DebugTracer = tracer }).ToFhirValues().ToList();
-            System.Diagnostics.Trace.WriteLine("Expression: " + expression);
             tracer.DumpDiagnostics();
 
             Assert.AreEqual(1, results.Count());
@@ -300,6 +501,9 @@ namespace Hl7.FhirPath.Tests
             Assert.AreEqual("20,9,constant: focus=1 result=1", tracer.traceOutput[4]);
             Assert.AreEqual("19,1,=: focus=1 result=1", tracer.traceOutput[5]);
             Assert.AreEqual("14,2,or: focus=1 result=1", tracer.traceOutput[6]);
+
+            // Now check the tracer assertions
+            tracer.Assert();
         }
     }
 }
