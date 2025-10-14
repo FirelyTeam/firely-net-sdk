078a3ce56e9e9e6331394100d2d84fdb7edbed0d
Andrzej Skowroński <andrzej@fire.ly>
2025-10-14 17:24:07 +0200

    Fix snapshot generator errors by correcting method references and updating outcome handling

--- src/Hl7.Fhir.Specification.STU3.Tests/DiscriminatorInterpreterTests.cs
+++ src/Hl7.Fhir.Specification.STU3.Tests/DiscriminatorInterpreterTests.cs
@@ -131,7 +131,6 @@ namespace Hl7.Fhir.Specification.Tests
         }
 
         [TestMethod]
-        [Ignore("Test resource has broken snapshot chain due to missing base profile")]
         public async Tasks.Task WalkToInlineComplexExtensionConstraints()
         {
             var sd = await _source.FindStructureDefinitionAsync("http://unittest.com/StructureDefinition/patient-sliced-complex-extension");
