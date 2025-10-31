9a057dbfc4d68341dfc24e5a444227704d79d9fe
Ewout Kramer <ewout@fire.ly>
2025-10-31 18:52:00 +0100

    Fixed more Rider errors in term services.

--- src/Hl7.Fhir.Base/Specification/Terminology/MimeTypeTerminologyService.cs
+++ src/Hl7.Fhir.Base/Specification/Terminology/MimeTypeTerminologyService.cs
@@ -2,39 +2,32 @@
 
 using System;
 
-namespace Hl7.Fhir.Specification.Terminology
+namespace Hl7.Fhir.Specification.Terminology;
+
+/// <summary>
+/// Checks if codes are valid Mime-Types (urn:ietf:bcp:13)
+/// </summary>
+public sealed class MimeTypeTerminologyService() : CustomValueSetTerminologyService("MIME type", MIMETYPE_SYSTEM,
+    [MIMETYPE_VALUESET_STU3, MIMETYPE_VALUESET_R4_AND_UP])
 {
-    /// <summary>
-    /// Checks if codes are valid Mime-Types (urn:ietf:bcp:13)
-    /// </summary>
-    public sealed class MimeTypeTerminologyService : CustomValueSetTerminologyService
+    private const string MIMETYPE_SYSTEM = "urn:ietf:bcp:13";
+    public const string MIMETYPE_VALUESET_R4_AND_UP = "http://hl7.org/fhir/ValueSet/mimetypes";
+    public const string MIMETYPE_VALUESET_STU3 = "http://www.rfc-editor.org/bcp/bcp13.txt";
+    private const string XML_CODE = "xml";
+    private const string JSON_CODE = "json";
+    private const string TTL_CODE = "ttl";
+
+    //mime-type format: type "/" [tree "."] subtype ["+" suffix]* [";" parameter];
+    //FHIR also allows for the following codes: xml, json, ttl
+    override protected bool ValidateCodeType(string code)
     {
-        private const string MIMETYPE_SYSTEM = "urn:ietf:bcp:13";
-        public const string MIMETYPE_VALUESET_R4_AND_UP = "http://hl7.org/fhir/ValueSet/mimetypes";
-        public const string MIMETYPE_VALUESET_STU3 = "http://www.rfc-editor.org/bcp/bcp13.txt";
-        private const string XML_CODE = "xml";
-        private const string JSON_CODE = "json";
-        private const string TTL_CODE = "ttl";
-
-        public MimeTypeTerminologyService() : base("MIME type", MIMETYPE_SYSTEM, [MIMETYPE_VALUESET_STU3, MIMETYPE_VALUESET_R4_AND_UP])
+        //This is a temporary fix until we support additional bindings.
+        if (code == XML_CODE || code == JSON_CODE || code == TTL_CODE)
         {
+            return true;
         }
 
-        //mime-type format: type "/" [tree "."] subtype ["+" suffix]* [";" parameter];
-        //FHIR also allows for the following codes: xml, json, ttl
-        override protected bool ValidateCodeType(string code)
-        {
-            //This is a temporary fix until we support additional bindings.
-            if (code == XML_CODE || code == JSON_CODE || code == TTL_CODE)
-            {
-                return true;
-            }
-
-            var entries = code.Split(new char[] { '/' }, StringSplitOptions.RemoveEmptyEntries);
-            return entries.Length == 2;
-        }
+        var entries = code.Split(['/'], StringSplitOptions.RemoveEmptyEntries);
+        return entries.Length == 2;
     }
 }
-
-#nullable restore
-
