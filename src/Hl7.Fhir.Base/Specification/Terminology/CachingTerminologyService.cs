4f29c0038c76b44c91182fbc29ca179f49c06c2c
Kasdejong <kas@fire.ly>
2025-12-02 14:43:09 +0100

    completed CachingTerminologyService.cs

--- src/Hl7.Fhir.Base/Specification/Terminology/CachingTerminologyService.cs
+++ src/Hl7.Fhir.Base/Specification/Terminology/CachingTerminologyService.cs
@@ -8,72 +8,129 @@ using System.Threading.Tasks;
 
 namespace Hl7.Fhir.Specification.Terminology;
 
-public class CachingTerminologyService(ITerminologyService terminologyService, MemoryCacheOptions? options = null, MemoryCacheEntryOptions? entryOptions = null) : ITerminologyService
+public class CachingTerminologyService : ITerminologyService
 {
-    private readonly IMemoryCache _cache = new MemoryCache(
-        options ?? new MemoryCacheOptions()
-        {
-            SizeLimit = 1024 * 1024 * 100, // 100 MB
-        }
-    );
-    
-    private readonly MemoryCacheEntryOptions _entryOptions = entryOptions ?? new MemoryCacheEntryOptions()
+    public CachingTerminologyService(ITerminologyService terminologyService, IMemoryCache cache, MemoryCacheEntryOptions? entryOptions = null)
     {
-        SlidingExpiration = TimeSpan.FromMinutes(30),
-        Size = 1 // Each entry is counted as size 1
+        _terminologyService = terminologyService;
+        _entryOptions = entryOptions ?? DEFAULT_ENTRY_OPTIONS;
+        _cache = cache;
+    }
+    
+    public CachingTerminologyService(ITerminologyService terminologyService, MemoryCacheOptions? cacheOptions = null, MemoryCacheEntryOptions? entryOptions = null)
+    {
+        _terminologyService = terminologyService;
+        _entryOptions = entryOptions ?? DEFAULT_ENTRY_OPTIONS;
+        _cache = new MemoryCache(cacheOptions ?? DEFAULT_CACHE_OPTIONS);
+    }
+
+    private static readonly MemoryCacheOptions DEFAULT_CACHE_OPTIONS = new MemoryCacheOptions()
+    {
+        SizeLimit = 1024 // Limit cache to 1024 entries
     };
     
+    private static readonly MemoryCacheEntryOptions DEFAULT_ENTRY_OPTIONS = new MemoryCacheEntryOptions()
+    {
+        SlidingExpiration = TimeSpan.FromMinutes(30), Size = 1 // Each entry is counted as size 1
+    };
+    
+    private readonly ITerminologyService _terminologyService;
+    
+    private readonly IMemoryCache _cache;
+
+    private readonly MemoryCacheEntryOptions _entryOptions;
+
     public Task<Parameters> ValueSetValidateCode(Parameters parameters, string? id = null, bool useGet = false)
     {
-        return _cache.GetOrCreate<Task<Parameters>>((parameters.GetHashCode(), id), entry =>
-        {
-            entry.SetOptions(_entryOptions);
-            return terminologyService.ValueSetValidateCode(parameters, id, useGet);
-        })!;
+        return parameters.GetParameterComponentHashCode() is { } hash
+            ? _cache.GetOrCreate<Task<Parameters>>(hash, entry =>
+                {
+                    entry.SetOptions(_entryOptions);
+                    return _terminologyService.ValueSetValidateCode(parameters, id, useGet);
+                })!
+            : _terminologyService.ValueSetValidateCode(parameters, id, useGet);
     } 
 
     public Task<Parameters> Subsumes(Parameters parameters, string? id = null, bool useGet = false)
     {
-        return _cache.GetOrCreate<Task<Parameters>>((parameters.GetHashCode(), id), entry =>
-        {
-            entry.SetOptions(_entryOptions);
-            return terminologyService.Subsumes(parameters, id, useGet);
-        })!;
+        return parameters.GetParameterComponentHashCode() is { } hash
+            ? _cache.GetOrCreate<Task<Parameters>>(hash, entry =>
+                {
+                    entry.SetOptions(_entryOptions);
+                    return _terminologyService.Subsumes(parameters, id, useGet);
+                })!
+            : _terminologyService.Subsumes(parameters, id, useGet);
     }
     
     public Task<Parameters> CodeSystemValidateCode(Parameters parameters, string? id = null, bool useGet = false)
     {
-        return _cache.GetOrCreate<Task<Parameters>>((parameters.GetHashCode(), id), entry =>
-        {
-            entry.SetOptions(_entryOptions);
-            return terminologyService.CodeSystemValidateCode(parameters, id, useGet);
-        })!;
+        return parameters.GetParameterComponentHashCode() is { } hash
+            ? _cache.GetOrCreate<Task<Parameters>>(hash, entry =>
+                {
+                    entry.SetOptions(_entryOptions);
+                    return _terminologyService.CodeSystemValidateCode(parameters, id, useGet);
+                })!
+            : _terminologyService.CodeSystemValidateCode(parameters, id, useGet);
     }
 
     public Task<Resource> Expand(Parameters parameters, string? id = null, bool useGet = false)
     {
-        return _cache.GetOrCreate<Task<Resource>>((parameters.GetHashCode(), id), entry =>
-        {
-            entry.SetOptions(_entryOptions);
-            return terminologyService.Expand(parameters, id, useGet);
-        })!;
+        return parameters.GetParameterComponentHashCode() is { } hash
+            ? _cache.GetOrCreate<Task<Resource>>(hash, entry =>
+                {
+                    entry.SetOptions(_entryOptions);
+                    return _terminologyService.Expand(parameters, id, useGet);
+                })!
+            : _terminologyService.Expand(parameters, id, useGet);
     }
     
     public Task<Parameters> Translate(Parameters parameters, string? id = null, bool useGet = false)
     {
-        return _cache.GetOrCreate<Task<Parameters>>((parameters.GetHashCode(), id), entry =>
-        {
-            entry.SetOptions(_entryOptions);
-            return terminologyService.Translate(parameters, id, useGet);
-        })!;
+        return parameters.GetParameterComponentHashCode() is { } hash
+            ? _cache.GetOrCreate<Task<Parameters>>(hash, entry =>
+                {
+                    entry.SetOptions(_entryOptions);
+                    return _terminologyService.Translate(parameters, id, useGet);
+                })!
+            : _terminologyService.Translate(parameters, id, useGet);
     }
     
     public Task<Resource> Closure(Parameters parameters, bool useGet = false)
     {
-        return _cache.GetOrCreate<Task<Resource>>(parameters.GetHashCode(), entry =>
+        return parameters.GetParameterComponentHashCode() is { } hash 
+            ? _cache.GetOrCreate<Task<Resource>>(hash, entry =>
+                {
+                    entry.SetOptions(_entryOptions);
+                    return _terminologyService.Closure(parameters, useGet);
+                })!
+            : _terminologyService.Closure(parameters, useGet);
+    }
+    
+}
+
+internal static class ParametersExtensions
+{
+    // returns null on resource parameters to avoid caching those. these are too complex for now.
+    public static int? GetParameterComponentHashCode(this Parameters parameters)
+    {
+        var hash = new HashCode();
+        foreach (var parameter in parameters.Parameter)
         {
-            entry.SetOptions(_entryOptions);
-            return terminologyService.Closure(parameters, useGet);
-        })!;
+            hash.Add(parameter.Name);
+            if (parameter.Value != null)
+                hash.Add(parameter.Value.GetHashCode());
+            if (parameter.Resource != null)
+                return null;
+            foreach (var part in parameter.Part)
+            {
+                hash.Add(part.Name);
+                if (part.Value != null)
+                    hash.Add(part.Value.GetHashCode());
+                if (part.Resource != null)
+                    return null;
+            }
+        }
+        return hash.ToHashCode();
     }
 }
+
