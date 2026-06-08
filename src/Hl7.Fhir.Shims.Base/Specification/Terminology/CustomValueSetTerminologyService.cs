1f73c9772d67049a2ecbd8e4be198c19b5b4b57d
Andrzej Skowroński <andrzej@fire.ly>
2026-06-08 09:27:03 +0200

    Relax resolve in base class to allow null and let caller decide if to throw

--- src/Hl7.Fhir.Shims.Base/Specification/Terminology/CustomValueSetTerminologyService.cs
+++ src/Hl7.Fhir.Shims.Base/Specification/Terminology/CustomValueSetTerminologyService.cs
@@ -46,7 +46,7 @@ public abstract class CustomValueSetTerminologyService : BaseTerminologyService
     /// </summary>
     /// <param name="canonical"></param>
     /// <returns></returns>
-    protected internal override Task<ValueSet> ResolveValueSet(Canonical canonical) => Task.FromResult(new ValueSet());
+    protected internal override Task<ValueSet?> ResolveValueSet(Canonical canonical) => Task.FromResult<ValueSet?>(new());
 
     /// <summary>
     /// Returns an uninitialized CodeSystem, as the implementations will verify only codes known to them. If CodeSystem is needed, override this method.
